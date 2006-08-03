152bcf3f2d692a532f4765336ade3b73be16811b
gunnar <gunnar@THAUMASIA>
2006-08-03 08:14:50 +0100

    better reporting...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 230342]

--- dist/win/generator_example.bat
+++ dist/win/generator_example.bat
@@ -10,23 +10,27 @@ echo Note that JAVADIR must be given *without* quotes
 :after_no_jni_h_in_javadir
 
 echo Running generator
-..\bin\generator mywidget.h typesystem_mywidget.txt
+..\bin\generator mywidget.h typesystem_mywidget.txt 2>&1 > log
 IF NOT "%ERRORLEVEL%" == "0" goto end
 
 echo Compiling Native library
-qmake
+qmake 2>&1 > log
 IF NOT "%ERRORLEVEL%" == "0" goto end
-nmake
+nmake 2>&1 > log
 IF NOT "%ERRORLEVEL%" == "0" goto end
 
 
 echo Compiling Java Sources
 cd ..
-javac -cp qtjambi.jar com\trolltech\examples\*.java com\trolltech\examples\generator\*.java 
+javac -cp qtjambi.jar com\trolltech\examples\*.java com\trolltech\examples\generator\*.java 2>&1 > log
 IF NOT "%ERRORLEVEL%" == "0" goto end
 
 
 echo Compilation done...
 echo You should now be able to run qtjambi.exe and run the GeneratorExample
+goto real_end
 
 :end
+type log
+
+:real_end
