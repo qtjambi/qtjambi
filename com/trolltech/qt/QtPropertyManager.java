2078eaee950dc24c66793d943dfa2d540175c069
qt <qt@manta>
2007-03-01 14:13:56 +0100

    minor debugging help
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 253423]

--- com/trolltech/qt/QtPropertyManager.java
+++ com/trolltech/qt/QtPropertyManager.java
@@ -6,23 +6,23 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class QtPropertyManager {
-    
+
     private static Class QT_PROPERTY_ANNOTATION_CLASSES[] = new Class[] {
             QtPropertyReader.class,
             QtPropertyWriter.class,
             QtPropertyResetter.class
     };
-    
+
     @SuppressWarnings("unused")
     private static void __qt_default_true() { }
-    
+
     @SuppressWarnings("unused")
     private static void __qt_default_false() { }
-    
+
     private static Method DEFAULT_TRUE = null;
     private static Method DEFAULT_FALSE = null;
-    
-    {        
+
+    {
         try {
             DEFAULT_TRUE = QtPropertyManager.class.getDeclaredMethod("__qt_default_true");
             DEFAULT_FALSE = QtPropertyManager.class.getDeclaredMethod("__qt_default_false");
@@ -31,35 +31,35 @@ public class QtPropertyManager {
         }
     }
 
-    public static class Entry {        
+    public static class Entry {
         public Entry(String name) { this.name = name; }
-        
+
         public String name;
-        
+
         public Method read;
         public Method write;
-        public Method reset;        
-        
+        public Method reset;
+
         public Method designable;
-        
+
         public int sortOrder;
-        
+
         boolean readable = true;
         boolean writable = true;
-        
+
         public boolean isDesignable(Object o) {
             if (designable == null || designable == DEFAULT_TRUE)
                 return true;
             else if (designable == DEFAULT_FALSE)
-                return false;            
+                return false;
             try {
                 return (Boolean) designable.invoke(o);
             } catch (Exception e) {
                 e.printStackTrace();
-            }                
+            }
             return false;
         }
-        
+
         public Class type() {
             if (read != null) return read.getReturnType();
             if (write != null) return write.getParameterTypes()[0];
@@ -68,79 +68,79 @@ public class QtPropertyManager {
     }
 
     private static String matchAndLowercase(String s, String prefix) {
-        if (s.length() == prefix.length() + 1) 
-            return String.valueOf(Character.toLowerCase(s.charAt(prefix.length()))); 
-        else 
+        if (s.length() == prefix.length() + 1)
+            return String.valueOf(Character.toLowerCase(s.charAt(prefix.length())));
+        else
             return Character.toLowerCase(s.charAt(prefix.length())) + s.substring(prefix.length()+1);
     }
-    
+
     private static String propertyNameForRead(QtPropertyReader r, Method m) {
-        String name = r.name();        
+        String name = r.name();
         if (name.length() == 0) {
             name = m.getName();
-            if (name.startsWith(("get"))) 
+            if (name.startsWith(("get")))
                 name = matchAndLowercase(name, "get");
             else if (m.getReturnType() == boolean.class || m.getReturnType() == Boolean.class) {
                 if (name.startsWith("is"))
                     name = matchAndLowercase(name, "is");
                 else if (name.startsWith("has"))
                     name = matchAndLowercase(name, "has");
-            }            
+            }
         }
         return name;
     }
-    
+
     private static String stripAndLowercase(String s, int length) {
-        if (s.length() == length + 1) 
-            return String.valueOf(Character.toLowerCase(s.charAt(length))); 
-        else 
+        if (s.length() == length + 1)
+            return String.valueOf(Character.toLowerCase(s.charAt(length)));
+        else
             return Character.toLowerCase(s.charAt(length)) + s.substring(length+1);
     }
-    
+
     private static String propertyNameForWrite(QtPropertyWriter r, Method m) {
-        String name = r.name();        
+        String name = r.name();
         if (name.length() == 0) {
             name = m.getName();
-            if (name.startsWith(("set"))) 
+            if (name.startsWith(("set")))
                 name = stripAndLowercase(name, 3);
         }
         return name;
     }
-    
+
     private static String propertyNameForReset(QtPropertyResetter r, Method m) {
-        String name = r.name();        
+        String name = r.name();
         if (name.length() == 0) {
             name = m.getName();
-            if (name.startsWith(("reset"))) 
+            if (name.startsWith(("reset")))
                 name = stripAndLowercase(name, 5);
         }
-        return name;        
+        return name;
     }
-    
+
     private static Method findMethod(Class<?> c, String name, Class ... args) {
         try { return c.getMethod(name, args); } catch (Exception e) { }
         return null;
-    }  
-    
+    }
+
     @SuppressWarnings("unchecked")
     private static boolean hasOtherAnnotations(Method m, Class expectedAnnotation) {
         for (Class c : QT_PROPERTY_ANNOTATION_CLASSES) {
             if (c != expectedAnnotation && m.isAnnotationPresent(c))
                 return true;
         }
-        
-        return false;    
+
+        return false;
     }
-    
-    private static Method findReadMethodForProperty(Class cl, String name, Class type) 
+
+    private static Method findReadMethodForProperty(Class cl, String name, Class type)
         throws QMalformedQtPropertyException {
-        
+
         // setX() / x() pair; Qt style
         Method getMethod = findMethod(cl, name);
-        
+
         if (getMethod == null) {
             name = upcaseFirst(name);
-            
+
             // setX() / getX(); Java style
             getMethod = findMethod(cl, "get" + name);
 
@@ -151,7 +151,7 @@ public class QtPropertyManager {
                     getMethod = findMethod(cl, "has" + name);
             }
         }
-        
+
         // Verify the type of the property...
         if (getMethod != null) {
             if (type != null && !type.equals(getMethod.getReturnType())) {
@@ -167,36 +167,36 @@ public class QtPropertyManager {
 
         return getMethod;
     }
-    
-    
-    
-    private static Method findWriteMethodForProperty(Class cl, String name, Class type) 
-        throws QMalformedQtPropertyException {  
+
+
+
+    private static Method findWriteMethodForProperty(Class cl, String name, Class type)
+        throws QMalformedQtPropertyException {
         name = upcaseFirst(name);
         Method m = findMethod(cl, "set" + name, type);
-        
+
         if (m != null) {
             // Verify that we don't have other annotation tags on this method.
             if (hasOtherAnnotations(m, QtPropertyWriter.class))
                 return null;
-        
+
             // Verify that the writer annotation matches this one...
             QtPropertyWriter wp = m.getAnnotation(QtPropertyWriter.class);
             if (wp != null && !wp.name().equals(name))
                 return null;
-            
+
             Class args[] = m.getParameterTypes();
             if (args.length != 1 || !args[0].equals(type))
                 return null;
         }
-        
+
         return m;
     }
 
     private static String upcaseFirst(String name) {
         return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
-    
+
     private static void checkDesignable(Entry e) {
         QtPropertyDesignable dp = e.read.getAnnotation(QtPropertyDesignable.class);
         if (dp != null) {
@@ -210,13 +210,13 @@ public class QtPropertyManager {
                     Method m = e.read.getDeclaringClass().getMethod(name);
                     if (m.getReturnType() == boolean.class)
                         e.designable = m;
-                } catch (Exception ex) { 
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         }
     }
-    
+
     private static int sortOrder(Method m) {
         QtPropertyOrder o = m.getAnnotation(QtPropertyOrder.class);
         return o != null ? o.value() : 0;
@@ -240,7 +240,7 @@ public class QtPropertyManager {
 
         return map;
     }
-    
+
     public static HashMap<String, Entry> findProperties(Class cl) throws QMalformedQtPropertyException {
         HashMap<String, Entry> entries = new HashMap<String, Entry>();
         Method methods[] = cl.getDeclaredMethods();
@@ -251,28 +251,28 @@ public class QtPropertyManager {
 
         // Match them up...
         for (Entry e : entries.values()) {
-            
+
             if (e.readable) {
                 if (e.read == null)
                     e.read = findReadMethodForProperty(cl, e.name, e.type());
             } else {
                 e.read = null;
             }
-            
+
             if (e.read != null) {
                 checkDesignable(e);
                 e.sortOrder = sortOrder(e.read);
             }
-            
-            
+
+
             if (e.writable) {
                 if (e.write == null)
-                    e.write = findWriteMethodForProperty(cl, e.name, e.type());    
+                    e.write = findWriteMethodForProperty(cl, e.name, e.type());
             } else {
                 e.write = null;
             }
         }
-        
+
 //            Entry e = entries.get(s);
 //            System.out.printf("entry: %s\n"
 //                    + " - readable=%s\n"
@@ -280,7 +280,7 @@ public class QtPropertyManager {
 //                    + " - reset=%s\n"
 //                    + " - designable=%s\n",
 //                    e.name, e.read, e.write, e.reset, e.designable);
-//        }        
+//        }
 //
 
         // Hardcode some classes for now...
@@ -299,15 +299,15 @@ public class QtPropertyManager {
             QtPropertyResetter reset = method.getAnnotation(QtPropertyResetter.class);
             if (reset == null)
                 continue;
-            
+
             String propertyName = propertyNameForReset(reset, method);
-            
+
             Entry e = entries.get(propertyName);
             if (e == null) {
                 e = new Entry(propertyName);
                 entries.put(propertyName, e);
             }
-            
+
             e.reset = method;
         }
     }
@@ -318,13 +318,13 @@ public class QtPropertyManager {
             if (write == null)
                 continue;
             String propertyName = propertyNameForWrite(write, method);
-            
+
             Entry e = entries.get(propertyName);
             if (e == null) {
                 e = new Entry(propertyName);
                 entries.put(propertyName, e);
             }
-            
+
             e.writable = write.enabled();
             e.write = method;
         }
@@ -334,30 +334,30 @@ public class QtPropertyManager {
         // Find all the annotated read methods
         for (Method method : methods) {
             QtPropertyReader read = method.getAnnotation(QtPropertyReader.class);
-            if (read == null) 
+            if (read == null)
                 continue;
             String propertyName = propertyNameForRead(read, method);
-            
+
             Entry e = entries.get(propertyName);
-            if (e != null) 
+            if (e != null)
                 throw new QMalformedQtPropertyException(propertyName, "Duplicate property: " + e.read, method);
-            
+
             e = new Entry(propertyName);
             e.readable = read.enabled();
             e.read = method;
-            
+
             entries.put(propertyName, e);
         }
     }
-    
+
     /**
      * Find and add all get/set pairs that match one of the following patterns.
-     * 
+     *
      * T getX() <-> setX(T)
      * T x()    <-> setX(T)
-     * 
+     *
      * and in addition if T is boolean:
-     * 
+     *
      * T isX()  <-> setX(T)
      * T hasX() <-> setX(T)
      *
@@ -442,14 +442,20 @@ public class QtPropertyManager {
             System.out.print(e.read != null ? " R" : "  ");
             System.out.print(e.write != null ? " W" : "  ");
             System.out.print(e.reset != null ? " =" : "  ");
-            System.out.print(e.isDesignable(null) ? " D" : "  ");
+
+            if (e.designable == DEFAULT_TRUE)
+                System.out.print(" D");
+            else if (e.designable != DEFAULT_FALSE && e.designable != null)
+                System.out.print(" *");
+            else
+                System.out.print("  ");
 
             Class cl = null;
             if (e.read != null)
                 cl = e.read.getDeclaringClass();
             else if (e.write != null)
                 cl = e.write.getDeclaringClass();
-            
+
             System.out.printf(" :: %30s ; " + cl, e.name);
 
             System.out.println();
