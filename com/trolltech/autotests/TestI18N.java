58376179ae2d57b7290d10dde989dffa5d69011d
hfroilan <a@b>
2007-04-10 16:33:38 +0100

    More tests
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 259015]

--- com/trolltech/autotests/TestI18N.java
+++ com/trolltech/autotests/TestI18N.java
@@ -3,8 +3,7 @@ package com.trolltech.autotests;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import com.trolltech.qt.core.QObject;
-import com.trolltech.qt.core.QTranslator;
+import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.QApplication;
 
 public class TestI18N extends QApplicationTest {
@@ -29,26 +28,29 @@ public class TestI18N extends QApplicationTest {
 	public class SimpleNotTranslated extends QObject {
 		public void testNotTranslated() {
 			assertEquals(tr("test"), "test");
-			assertEquals(tr("not translated øæå"), "not translated øæå");
+			assertEquals(tr("\u03c0"), "\u03c0"); //  Pi
+			assertEquals(tr("\u06a0"), "\u06a0"); //  Arabic
+			assertEquals(tr("not translated ø"), "not translated ø");
 			assertEquals(tr("not translated \u00e8"), "not translated è");
 			assertEquals(tr("Hello è"), "Hello \u00e8");
-			assertEquals(tr("not translated 123"), "not translated 123");
+			assertEquals(tr("not translated \n123"), "not translated \n123");
 		}
 	}
 	
 	@Test
 	public void TestSimpleTranslated() {
 		QApplication.installTranslator(null);
-		SimpleNotTranslated test = new SimpleNotTranslated();
-		test.testNotTranslated();
+		SimpleTranslated test = new SimpleTranslated();
+		test.testTranslated();
 	}
 	
 	@Test
 	public void TestSimpleTranslatedWithTranslationsLoaded() {
 		QTranslator translator = new QTranslator();
+		
 		assertTrue( translator.load("classpath:com/trolltech/autotests/i18n.qm"));
 	    QApplication.installTranslator(translator);
-		
+	    	    
 		SimpleTranslated test = new SimpleTranslated();
 		test.testTranslated();
 	}
@@ -56,7 +58,14 @@ public class TestI18N extends QApplicationTest {
 	public class SimpleTranslated extends QObject {
 		public void testTranslated() {
 			assertEquals(tr("test"), "test");
+			assertEquals(tr("one\ntwo"), "en\nto");
+			assertEquals(tr("abc-ø-abc"), "abc-ø-abc"); // e8
+			assertEquals(tr("\u00e8"), "ok"); // e8
+			assertEquals(tr("\u03c0"), "ok Pi"); //  Pi
+			assertEquals(tr("\u06a0"), "ok Arabisk"); //  Arabic
+			assertEquals(tr("è"), "ok"); // e8
 			assertEquals(tr("not translated abc"), "ikke oversatt æøå");
+			assertEquals(tr("not translated æøå"), "ikke oversatt æøå");
 			assertEquals(tr("not translated 2 \u00e8"), "ikke oversatt 2 è");
 			assertEquals(tr("Hello è"), "Halloys \u00e8");
 			assertEquals(tr("My mother 123"), "Min mor 123");
--- com/trolltech/autotests/i18n.ts
+++ com/trolltech/autotests/i18n.ts
@@ -3,58 +3,96 @@
 <defaultcodec></defaultcodec>
 <context encoding="UTF-8">
     <name>com.trolltech.autotests.TestI18N$SimpleNotTranslated</name>
-    <message>
-        <location filename="TestI18N.java" line="31"/>
-        <source>test</source>
-        <translation type="unfinished"></translation>
-    </message>
     <message encoding="UTF-8">
-        <location filename="TestI18N.java" line="32"/>
-        <source>not translated øæå</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message encoding="UTF-8">
-        <location filename="TestI18N.java" line="33"/>
-        <source>not translated è</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message encoding="UTF-8">
-        <location filename="TestI18N.java" line="34"/>
+        <location filename="TestI18N.java" line="35"/>
         <source>Hello è</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="TestI18N.java" line="35"/>
-        <source>not translated 123</source>
+        <location filename="TestI18N.java" line="30"/>
+        <source>test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="33"/>
+        <source>not translated ø</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="34"/>
+        <source>not translated è</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TestI18N.java" line="36"/>
+        <source>not translated 
+123</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context encoding="UTF-8">
     <name>com.trolltech.autotests.TestI18N$SimpleTranslated</name>
     <message>
-        <location filename="TestI18N.java" line="58"/>
-        <source>test</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="TestI18N.java" line="59"/>
+        <location filename="TestI18N.java" line="67"/>
         <source>not translated abc</source>
         <translation>ikke oversatt æøå</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="TestI18N.java" line="60"/>
+        <location filename="TestI18N.java" line="69"/>
         <source>not translated 2 è</source>
         <translation>ikke oversatt 2 è</translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="TestI18N.java" line="61"/>
+        <location filename="TestI18N.java" line="70"/>
         <source>Hello è</source>
         <translation>Halloys è</translation>
     </message>
     <message>
-        <location filename="TestI18N.java" line="62"/>
+        <location filename="TestI18N.java" line="71"/>
         <source>My mother 123</source>
         <translation>Min mor 123</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="68"/>
+        <source>not translated æøå</source>
+        <translation>ikke oversatt æøå</translation>
+    </message>
+    <message>
+        <location filename="TestI18N.java" line="61"/>
+        <source>one
+two</source>
+        <translation>en
+to</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="66"/>
+        <source>è</source>
+        <translation>ok</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="65"/>
+        <source>ø</source>
+        <translation type="obsolete">ok ø</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="64"/>
+        <source>π</source>
+        <translation>ok Pi</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="65"/>
+        <source>ڠ</source>
+        <translation>ok Arabisk</translation>
+    </message>
+    <message>
+        <location filename="TestI18N.java" line="60"/>
+        <source>test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="TestI18N.java" line="62"/>
+        <source>abc-ø-abc</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
