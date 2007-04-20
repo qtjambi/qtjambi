ab3b0bf343ec843901f1471bc8d0e5fba7a3abc6
qt-user <qt-info@nokia.com>
2007-04-20 15:26:59 +0100

    Ported build_javadoc.bat to build_javadoc.sh
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 260888]

---
+++ scripts/build_javadoc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if  [ "$QTJAMBI_VERSION" = "" ]; then
+     export QTJAMBI_VERSION=main
+fi
+
+if [ "$QDOC" = "" ]; then
+    if [ "$QTDIR" = "" ]; then
+        echo "Unable to find qdoc3. Set the QDOC or QTDIR environment variables."
+        exit 1
+    fi
+    export LOCAL_QDOC=$QTDIR/util/qdoc3
+else
+    export LOCAL_QDOC=$QDOC
+fi
+
+echo "qdoc3 found in: $LOCAL_QDOC"
+
+export JAMBI=$PWD/..
+
+# Clean the directory
+cd  $JAMBI/doc
+rm -Rf *
+mkdir html
+
+# Generating the QDoc JAPI file
+cd $JAMBI/generator
+./generator --build-qdoc-japi
+
+# Running QDoc
+cd $LOCAL_QDOC/test
+../qdoc3 qt-for-jambi.qdocconf jambi.qdocconf
+
+cd $JAMBI/doc/html/com/trolltech/qt
+jar -cf $JAMBI/scripts/qtjambi-jdoc-$QTJAMBI_VERSION.jar *.jdoc
+
+# Generating the sourcecode
+# cd $JAMBI/generator
+cd $JAMBI/generator
+./generator --jdoc-enabled --jdoc-dir ../doc/html/com/trolltech/qt
+
+# Generating the Javadoc
+cd $JAMBI/doc/html
+javadoc -J-Xmx500m -sourcepath $JAMBI com.trolltech.qt com.trolltech.qt.core com.trolltech.qt.gui com.trolltech.qt.opengl com.trolltech.qt.sql com.trolltech.qt.opengl com.trolltech.qt.svg com.trolltech.qt.network com.trolltech.qt.xml com.trolltech.qt.designer
+
+jar -cf qtjambi-javadoc-$QTJAMBI_VERSION.jar *
+
+cd $JAMBI/scripts
+cp $JAMBI/doc/html/qtjambi-javadoc-$QTJAMBI_VERSION.jar .
+
+export LOCAL_QDOC=
