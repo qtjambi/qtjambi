6038eee8dfdf02c7be890b613489864746e08aa5
eblomfel <eblomfel@SpareRibs>
2007-05-10 02:37:32 +0100

    Drop shadow. Modified code from newform.cpp in Designer.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 263409]

--- com/trolltech/launcher/SplashScreen.java
+++ com/trolltech/launcher/SplashScreen.java
@@ -5,7 +5,7 @@ import com.trolltech.qt.gui.*;
 
 class SplashScreen extends QSplashScreen {
 
-    private static final QSize SIZE = new QSize(400, 200);
+    private static final QSize SIZE = new QSize(600, 300);
 
     public SplashScreen() {
         QRect r = QApplication.desktop().rect();
@@ -14,13 +14,16 @@ class SplashScreen extends QSplashScreen {
 		                                   r.height() / 2 - SIZE.height() / 2,
 		                                   SIZE.width(), SIZE.height());
 
-        QPixmap target = new QPixmap(SIZE);
+        QImage target = new QImage(SIZE, QImage.Format.Format_ARGB32_Premultiplied);
         QPixmap logo = new QPixmap("classpath:com/trolltech/images/logo.png");
-        QRectF tr = new QRectF(0, 0, SIZE.width(), SIZE.height());
+        
+        final int margin = 20;
+        final int shadow = 50;        
+        QRectF tr = new QRectF(0, 0, SIZE.width() - margin - shadow -1 , SIZE.height() - margin - shadow -1);
 
-        int round = 40;
-        int round2 = round * 2;
-
+        final int round = 40;
+        final int round2 = round * 2;
+        
         QPainterPath path = new QPainterPath();
         path.moveTo(tr.left() + round, tr.top());
         path.lineTo(tr.right() - round, tr.top());
@@ -48,9 +51,73 @@ class SplashScreen extends QSplashScreen {
         p.drawPixmap(1, -1, desktopBg);
         p.drawPixmap(-1, 1, desktopBg);
         p.setOpacity(0.5);
-        p.fillRect(tr, new QBrush(QColor.white));
+        p.fillRect(target.rect(), new QBrush(QColor.white));
         p.restore();
         
+        final QRectF rectRight = new QRectF(tr.left() - margin + tr.width() + 1, 
+				   tr.top() + shadow, 
+				   shadow, tr.height() - shadow + 1 - margin);
+        final QRectF rectBottom = new QRectF(tr.left() + shadow, 
+        		tr.top() - margin + tr.height() + 1, tr.width() - shadow + 1 - margin, shadow);
+        final QRectF rectBottomRight = new QRectF(tr.left() - margin + tr.width() + 1, 
+        		tr.top() - margin + tr.height() + 1, shadow, shadow);
+        final QRectF rectTopRight = new QRectF(tr.left() - margin + tr.width() + 1, 
+        		tr.top(), shadow, shadow);
+        final QRectF rectBottomLeft = new QRectF(tr.left(), 
+        		tr.top() - margin + tr.height() + 1, shadow, shadow);
+        
+        QPainterPath clipPath = new QPainterPath();
+        clipPath.addRect(new QRectF(0, 0, SIZE.width(), SIZE.height()));        
+        clipPath.addPath(path);
+        
+        p.setClipPath(clipPath);
+        
+        final QColor dark = QColor.darkGray;
+        final QColor light = QColor.transparent;               
+       
+        // Drop shadow: right shadow
+        {
+            QLinearGradient lg = new QLinearGradient(rectRight.topLeft(), 
+            		                                 rectRight.topRight());
+            lg.setColorAt(0, dark);
+            lg.setColorAt(1, light);
+            p.fillRect(rectRight, new QBrush(lg));
+        }
+
+        // bottom shadow
+        {
+            QLinearGradient lg = new QLinearGradient(rectBottom.topLeft(), rectBottom.bottomLeft());
+            lg.setColorAt(0, dark);
+            lg.setColorAt(1, light);
+            p.fillRect(rectBottom, new QBrush(lg));
+        }
+
+        // bottom/right corner shadow
+        {
+            QRadialGradient g = new QRadialGradient(rectBottomRight.topLeft(), shadow);
+            g.setColorAt(0, dark);
+            g.setColorAt(1, light);
+            p.fillRect(rectBottomRight, new QBrush(g));
+        }
+
+        // top/right corner
+        {
+            QRadialGradient g = new QRadialGradient(rectTopRight.bottomLeft(), shadow);
+            g.setColorAt(0, dark);
+            g.setColorAt(1, light);
+            p.fillRect(rectTopRight, new QBrush(g));
+        }
+
+        // bottom/left corner
+        {
+            QRadialGradient g = new QRadialGradient(rectBottomLeft.topRight(), shadow);
+            g.setColorAt(0, dark);
+            g.setColorAt(1, light);
+            p.fillRect(rectBottomLeft, new QBrush(g));
+        }
+
+        p.setClipPath(path);
+
         // The border
         for (int i=30; i>=0; i-=3) {
             int intensity = i * 255 / 30;
@@ -63,6 +130,6 @@ class SplashScreen extends QSplashScreen {
 
         p.end();
 
-        setPixmap(target);
+        setPixmap(QPixmap.fromImage(target));
     }
 }
