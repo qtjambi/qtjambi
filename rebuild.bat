b88cc4a5c3b5178e532e196c50c0208c11b40e33
gunnar <gunnar@THAUMASIA>
2007-05-15 15:25:25 +0100

    make rebuild work despite the pch problems...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 264100]

--- rebuild.bat
+++ rebuild.bat
@@ -5,11 +5,13 @@ if "%qmakespec%" == "win32-g++" set MAKE_TOOL=mingw32-make
 p4 sync ...
 cd generator
 qmake 
+%make_tool% clean
 %make_tool%
 release\generator || debug\generator
 cd ..
 del makefil* /s
-qmake -r -config sanitycheck
+qmake -r -config sanitycheck 
+%MAKE_TOOL% clean
 %MAKE_TOOL%
 bin\juic -cp . -a -e eclipse-stable\
 javac -J-mx1024m @java_files
@@ -19,6 +21,7 @@ REM autotest stuff
 cd autotestlib
 call build
 qmake
+%MAKE_TOOL% clean
 %MAKE_TOOL%
 cd ..
 javac com\trolltech\autotests\*.java com\trolltech\autotests\generated\*.java
@@ -28,6 +31,7 @@ REM generator example
 cd generator_example
 ..\generator\release\generator global.h typesystem_generatorexample.txt
 qmake
+%MAKE_TOOL% clean
 %MAKE_TOOL%
 cd ..
 javac com\trolltech\examples\generator\*.java com\trolltech\examples\GeneratorExample.java
