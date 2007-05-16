a82b2f0e9ddbbcbf3093f1621e232b423bafadca
gunnar <gunnar@THAUMASIA>
2007-05-16 10:02:10 +0100

    SplashScreen into java_files and compile
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 264181]

--- com/trolltech/launcher/SplashScreen.java
+++ com/trolltech/launcher/SplashScreen.java
@@ -8,24 +8,30 @@ class SplashScreen extends QSplashScreen {
     private static final QSize SIZE = new QSize(600, 300);
     private QPixmap cleanSplash;
     private QPixmap logo;
+    private QRect splashScreenRect;
 
     public SplashScreen() {
         QRect r = QApplication.desktop().rect();
+        splashScreenRect = new QRect(r.width() / 2 - SIZE.width() / 2,
+                                     r.height() / 2 - SIZE.height() / 2,
+                                     SIZE.width(), SIZE.height());
+
         QPixmap desktopBg = QPixmap.grabWindow(QApplication.desktop().winId(),
-		                                   r.width() / 2 - SIZE.width() / 2,
-		                                   r.height() / 2 - SIZE.height() / 2,
-		                                   SIZE.width(), SIZE.height());
+                                               splashScreenRect.x(),
+                                               splashScreenRect.y(),
+                                               splashScreenRect.width(),
+                                               splashScreenRect.height());
 
         QImage target = new QImage(SIZE, QImage.Format.Format_ARGB32_Premultiplied);
         logo = new QPixmap("classpath:com/trolltech/images/logo.png");
-        
+
         final int margin = 20;
-        final int shadow = 50;        
+        final int shadow = 50;
         QRectF tr = new QRectF(0, 0, SIZE.width() - margin - shadow -1 , SIZE.height() - margin - shadow -1);
 
         final int round = 40;
         final int round2 = round * 2;
-        
+
         QPainterPath path = new QPainterPath();
         path.moveTo(tr.left() + round, tr.top());
         path.lineTo(tr.right() - round, tr.top());
@@ -53,50 +59,50 @@ class SplashScreen extends QSplashScreen {
         p.drawPixmap(1, -1, desktopBg);
         p.drawPixmap(-1, 1, desktopBg);
         p.setOpacity(1);
-        
+
         QLinearGradient gradient = new QLinearGradient(0, 0, 0, tr.height());
-        
+
         double alpha = 0.3;
         QColor whiteAlpha = QColor.fromRgbF(1, 1, 1, alpha);
         QColor whiteHighlight = QColor.fromRgbF(1, 1, 1);
         QColor whiteBorder = QColor.fromRgbF(0.5, 0.5, 0.5, 0.8);
-        
+
         double highlight = 0.84;
         double size = 0.08;
-        
+
         gradient.setColorAt(highlight - size, whiteAlpha);
         gradient.setColorAt(highlight - size*0.9, whiteBorder);
         gradient.setColorAt(highlight, whiteHighlight);
         gradient.setColorAt(highlight + size*0.9, whiteBorder);
         gradient.setColorAt(highlight + size, whiteAlpha);
-        
+
         p.fillRect(target.rect(), new QBrush(gradient));
         p.restore();
-        
-        final QRectF rectRight = new QRectF(tr.left() - margin + tr.width() + 1, 
-				   tr.top() + shadow, 
+
+        final QRectF rectRight = new QRectF(tr.left() - margin + tr.width() + 1,
+				   tr.top() + shadow,
 				   shadow, tr.height() - shadow + 1 - margin);
-        final QRectF rectBottom = new QRectF(tr.left() + shadow, 
+        final QRectF rectBottom = new QRectF(tr.left() + shadow,
         		tr.top() - margin + tr.height() + 1, tr.width() - shadow + 1 - margin, shadow);
-        final QRectF rectBottomRight = new QRectF(tr.left() - margin + tr.width() + 1, 
+        final QRectF rectBottomRight = new QRectF(tr.left() - margin + tr.width() + 1,
         		tr.top() - margin + tr.height() + 1, shadow, shadow);
-        final QRectF rectTopRight = new QRectF(tr.left() - margin + tr.width() + 1, 
+        final QRectF rectTopRight = new QRectF(tr.left() - margin + tr.width() + 1,
         		tr.top(), shadow, shadow);
-        final QRectF rectBottomLeft = new QRectF(tr.left(), 
+        final QRectF rectBottomLeft = new QRectF(tr.left(),
         		tr.top() - margin + tr.height() + 1, shadow, shadow);
-        
+
         QPainterPath clipPath = new QPainterPath();
-        clipPath.addRect(new QRectF(0, 0, SIZE.width(), SIZE.height()));        
+        clipPath.addRect(new QRectF(0, 0, SIZE.width(), SIZE.height()));
         clipPath.addPath(path);
-        
+
         p.setClipPath(clipPath);
-        
+
         final QColor dark = QColor.black;
-        final QColor light = QColor.transparent;               
-       
+        final QColor light = QColor.transparent;
+
         // Drop shadow: right shadow
         {
-            QLinearGradient lg = new QLinearGradient(rectRight.topLeft(), 
+            QLinearGradient lg = new QLinearGradient(rectRight.topLeft(),
             		                                 rectRight.topRight());
             lg.setColorAt(0, dark);
             lg.setColorAt(1, light);
@@ -152,26 +158,30 @@ class SplashScreen extends QSplashScreen {
         cleanSplash = QPixmap.fromImage(target);
         setPixmap(cleanSplash);
     }
-    
+
+    public QRect splashScreenRect() {
+        return splashScreenRect;
+    }
+
     @SuppressWarnings("unused")
     private void updateProgress(String progressMessage) {
-    	QPixmap pm = cleanSplash.copy();    	
-    	QPainter p = new QPainter(pm); 
-    	
+    	QPixmap pm = cleanSplash.copy();
+    	QPainter p = new QPainter(pm);
+
     	QFont font = p.font();
     	font.setPixelSize(11);
     	p.setFont(font);
-    	
+
     	QFontMetrics metrics = new QFontMetrics(font);
     	double textWidth = metrics.width(progressMessage);
     	double textHeight = metrics.ascent();
-    	
+
     	QRectF rect = new QRectF(logo.rect().adjusted(10, 0, 0, 0));
     	p.drawText(rect, progressMessage, new QTextOption(new Qt.Alignment(Qt.AlignmentFlag.AlignLeft, Qt.AlignmentFlag.AlignBottom)));
     	p.end();
-    	
-    	setPixmap(pm);        	
+
+    	setPixmap(pm);
     }
-    
-    
+
+
 }
