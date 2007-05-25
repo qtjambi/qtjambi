b3ed6de8d09a74c32bae20128a3e0b8e3110fef7
qt <qt@manta>
2007-05-25 17:46:06 +0100

    Username: gunnar package-downloader fixes for windows...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 265594]

--- scripts/package_download_windows.bat
+++ scripts/package_download_windows.bat
@@ -1,11 +1,11 @@
 pushd d:\tmp
 
 set OLD_PATH=%path%
+set OLD_QMAKESPEC=%qmakespec%
 set QT_VERSION=4.3.0
 set QT_COMMERCIAL_PACKAGE=qt-win-commercial-src-%QT_VERSION%
 set QT_GPL_PACKAGE=qt-win-opensource-src-%QT_VERSION%
 set QT_EVAL_PACKAGE=qt-win-evalpatches-src-%QT_VERSION%
-
 
 rm %QT_COMMERCIAL_PACKAGE%.zip
 rm %QT_GPL_PACKAGE%.zip
@@ -24,8 +24,9 @@ unzip %QT_COMMERCIAL_PACKAGE%.zip > log
 unzip %QT_EVAL_PACKAGE%.zip > log
 mv %QT_COMMERCIAL_PACKAGE% qt-eval-%QT_VERSION%
 cd qt-eval-%QT_VERSION%
-echo empty>LICENSE.TROLL
-configure -no-qt3support -release -shared -no-dsp -no-vcproj -DQT_EVAL 
+echo Trolltech employees and agents use this software under authority> LICENSE.TROLL
+echo from Trolltech ASA of Norway.>> LICENSE.TROLL
+configure -no-qt3support -release -shared -no-dsp -no-vcproj -D QT_EVAL 
 nmake sub-src sub-tools
 nmake clean
 cd ..
@@ -39,7 +40,8 @@ rm -rf qt-commercial-%QT_VERSION%
 unzip %QT_COMMERCIAL_PACKAGE%.zip > log
 mv %QT_COMMERCIAL_PACKAGE% qt-commercial-%QT_VERSION%
 cd qt-commercial-%QT_VERSION%
-echo empty>LICENSE.TROLL
+echo Trolltech employees and agents use this software under authority> LICENSE.TROLL
+echo from Trolltech ASA of Norway.>> LICENSE.TROLL
 configure -no-qt3support -release -shared -no-dsp -no-vcproj
 nmake sub-src sub-tools
 nmake clean
@@ -51,11 +53,13 @@ REM
 REM Open Source packages
 REM 
 rm -rf qt-opensource-%QT_VERSION%
-unzip %QT_OPENSOURCE_PACKAGE%.zip > log
-mv %QT_OPENSOURCE_PACKAGE% qt-opensource-%QT_VERSION%
+unzip %QT_GPL_PACKAGE%.zip > log
+mv %QT_GPL_PACKAGE% qt-opensource-%QT_VERSION%
 set PATH=c:\mingw\bin
 cd qt-opensource-%QT_VERSION%
-configure -no-qt3support -release -shared -no-dsp -no-vcproj 
+set QMAKESPEC=win32-g++
+echo yes> .tmp
+configure -no-qt3support -release -shared -no-dsp -no-vcproj < .tmp
 mingw32-make sub-src sub-tools
 mingw32-make clean
 cd ..
@@ -67,4 +71,5 @@ REM
 REM Clean up
 REM 
 set PATH=%OLD_PATH%
+set QMAKESPEC=%OLD_QMAKESPEC%
 popd
