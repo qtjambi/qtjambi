170fc2a24d52e0e2b931b8c13884682d726725ec
qt-user <qt-info@nokia.com>
2007-06-19 11:54:12 +0100

    Digital Clock example to Jambi
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 267855]

---
+++ com/trolltech/examples/DigitalClock.java
@@ -0,0 +1,41 @@
+package com.trolltech.examples;
+
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.*;
+
+@QtJambiExample(name = "DigitalClock")
+public class DigitalClock extends QLCDNumber
+{
+    public DigitalClock()
+    {
+        setSegmentStyle(SegmentStyle.Filled);
+
+        QTimer timer = new QTimer(this);
+        timer.timeout.connect(this, "showTime()");
+        timer.start(1000);
+
+        showTime();
+
+        setWindowTitle(tr("Digital Clock"));
+        resize(150, 60);
+    }
+
+    public void showTime()
+    {
+        QTime time = QTime.currentTime();
+        StringBuffer text = new StringBuffer(time.toString("hh:mm"));
+        if ((time.second() % 2) == 0)
+            text.setCharAt(2, ' ');
+        display(text.toString());
+    }
+
+    public static void main(String args[])
+    {
+        QApplication.initialize(args);
+
+        new DigitalClock().show();
+
+        QApplication.exec();
+    }
+}
