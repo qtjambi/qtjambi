634f226980bfdcfd558d437ce0a00e697a3de0c4
eblomfel <eblomfel@SpareRibs>
2007-07-03 14:37:59 +0100

    Integrate changes to qawtwidget
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 269228]

--- jawt/qawtwidget.cpp
+++ jawt/qawtwidget.cpp
@@ -20,94 +20,97 @@
 
 #include <windows.h>
 
-extern "C" JNIEXPORT void JNICALL Java_com_trolltech_extensions_awt_QAwtWidget_paintIt
-(JNIEnv *env, jobject widget, jobject graphics) 
+char *qtjambi_awt_title = "Qt Jambi/AWT";
+
+extern "C" JNIEXPORT void JNICALL Java_com_trolltech_awt_QAwtWidget_paint
+(JNIEnv *env, jobject widget, jobject ) 
 {
     JAWT awt;
-    JAWT_DrawingSurface *ds;
-    JAWT_Win32DrawingSurfaceInfo *windsi;
-    JAWT_DrawingSurfaceInfo *dsi;
     awt.version = JAWT_VERSION_1_3;
     if (JAWT_GetAWT(env, &awt) == JNI_FALSE) {
-        printf("AWT not found\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't get JAWT interface\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
-
-    ds = awt.GetDrawingSurface(env, widget);
-    if (ds == 0) {
-        printf("NULL!!!");
-        return;
-    }
-
-    jint lock = ds->Lock(ds);
-    if (lock & JAWT_LOCK_ERROR) {
-        printf("Error");
-        return;
-    }
-    
-    dsi = ds->GetDrawingSurfaceInfo(ds);
-    windsi = (JAWT_Win32DrawingSurfaceInfo*) dsi->platformInfo;
 
     jclass clazz = env->GetObjectClass(widget);
     if (clazz == 0) {
-        printf("NO CLASS\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't get JNI class interface of widget\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
     jfieldID id = env->GetFieldID(clazz, "containedWidget", "Lcom/trolltech/qt/gui/QWidget;");
     if (id == 0) {
-        printf("No id\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't get JNI field ID of field 'containedWidget'\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
     jobject qtWidget = env->GetObjectField(widget, id);
     if (qtWidget == 0) {
-        printf("No Qt Widget\n");
+        fprintf(stderr, "%s [%s:%d]: Unexpected null value in field 'containedWidget'\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
     QWidget *w = (QWidget *) qtjambi_to_qobject(env, qtWidget);
     if (w == 0) {
-        printf("Couldn't convert to QWidget\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't convert contained widget to C++ object\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
-
-    //QPixmap pm = QPixmap::grabWidget(w);
-
     jfieldID widgetAppearanceId = env->GetFieldID(clazz, "widgetAppearance", "Lcom/trolltech/qt/gui/QPixmap;");
     if (widgetAppearanceId == 0) {
-        printf("Can't get widget appearance\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't get JNI field ID of field 'widgetAppearance'\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
     jobject widgetAppearance = env->GetObjectField(widget, widgetAppearanceId);
     if (widgetAppearance == 0) {
-        printf("widget apperance not set\n");    
+        fprintf(stderr, "%s [%s:%d]: Unexpected null value in field 'widgetAppearance'\n", qtjambi_awt_title, __FILE__, __LINE__);
+        return;
     }
 
     QPixmap *pm = (QPixmap *) qtjambi_to_object(env, widgetAppearance);
     if (pm == 0) {
-        printf("Can't convert widget appearance\n");
+        fprintf(stderr, "%s [%s:%d]: Couldn't convert widget appearance to C++ object\n", qtjambi_awt_title, __FILE__, __LINE__);
         return;
     }
 
-    HBITMAP hbitmap = pm->toWinHBITMAP(QPixmap::NoAlpha);
-    fprintf(stderr, "Pixmap size: %d, %d\n", pm->width(), pm->height()); 
+    {
+        JAWT_DrawingSurface *ds;
+        ds = awt.GetDrawingSurface(env, widget);
+        if (ds == 0) {
+            fprintf(stderr, "%s [%s:%d]: Couldn't get drawing surface\n", qtjambi_awt_title, __FILE__, __LINE__);
+            return;
+        }
 
-    HDC hdc = windsi->hdc;
-    HDC hbitmap_hdc = CreateCompatibleDC(qt_win_display_dc());
-    HGDIOBJ null_bitmap = SelectObject(hbitmap_hdc, hbitmap);
-    if (!BitBlt(hdc, 0, 0, pm->width(), pm->height(), hbitmap_hdc, 0, 0, SRCCOPY)) {
-        printf("Goddamn you GDI\n");
-    }
-    SelectObject(hbitmap_hdc, null_bitmap);
-    DeleteObject(hbitmap);
-    DeleteObject(hbitmap_hdc);
+        jint lock = ds->Lock(ds);
+        if (lock & JAWT_LOCK_ERROR) {
+            fprintf(stderr, "%s [%s:%d]: Couldn't lock drawing surface\n", qtjambi_awt_title, __FILE__, __LINE__);
+            return;
+        }
 
+        {
+            HDC hdc = 0;
+            JAWT_DrawingSurfaceInfo *dsi = ds->GetDrawingSurfaceInfo(ds);
+            if (dsi == 0) {
+                fprintf(stderr, "%s [%s:%d]: Couldn't get drawing surface info\n", qtjambi_awt_title, __FILE__, __LINE__);
+                return;
+            }
 
-    ds->FreeDrawingSurfaceInfo(dsi);
-    ds->Unlock(ds);
-    awt.FreeDrawingSurface(ds);
-    
+            hdc = reinterpret_cast<JAWT_Win32DrawingSurfaceInfo *>(dsi->platformInfo)->hdc;
+        
+            HDC hbitmap_hdc = CreateCompatibleDC(qt_win_display_dc());        
+            HGDIOBJ null_bitmap = SelectObject(hbitmap_hdc, pm->toWinHBITMAP(QPixmap::NoAlpha));
+
+            if (!BitBlt(hdc, 0, 0, pm->width(), pm->height(), hbitmap_hdc, 0, 0, SRCCOPY)) {
+                fprintf(stderr, "%s [%s:%d]: Unable to blit to HDC: %p\n", qtjambi_awt_title, __FILE__, __LINE__, hdc);
+            }
+
+            DeleteObject(SelectObject(hdc, null_bitmap));
+            DeleteDC(hbitmap_hdc);
+            ds->FreeDrawingSurfaceInfo(dsi);
+        }
+
+        ds->Unlock(ds);
+        awt.FreeDrawingSurface(ds);
+    }    
 }
