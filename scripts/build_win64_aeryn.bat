55dad04869afcb9ab03816f10ced29686497cfc0
gunnar <gunnar@THAUMASIA>
2007-09-28 07:28:36 +0100

    p4i integration Integrate 278336 from 4.3 to main: don't do gpl for win64
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 278338]

--- scripts/build_win64_aeryn.bat
+++ scripts/build_win64_aeryn.bat
@@ -1,4 +1,5 @@
-call build --qt-gpl d:\tmp\qt-opensource --qt-commercial d:\tmp\qt-commercial --qt-eval d:\tmp\qt-eval --qt d:\tmp\qt-commercial %1 %2 %3 %4 %5 %6 %7 %8 %9
+call build --no-gpl --qt-commercial c:\tmp\qt-commercial --qt-eval c:\tmp\qt-eval --qt c:\tmp\qt-commercial %1 %2 %3 %4 %5 %6 %7 %8 %9
+
 scp qtjambi*eval* gunnar@anarki:~/public_html/packages/eval
 scp qtjambi*gpl* gunnar@anarki:~/public_html/packages/gpl
 scp qtjambi*commerc* gunnar@anarki:~/public_html/packages/commercial
