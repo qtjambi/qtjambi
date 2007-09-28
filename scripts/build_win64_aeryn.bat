27fbedbe95d4dbee4ea7beaf3d0a196dbc446825
gunnar <gunnar@THAUMASIA>
2007-09-28 07:28:25 +0100

    p4i integration Integrate 272759 from 4.3 to main: package building stuff
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 278337]

--- scripts/build_win64_aeryn.bat
+++ scripts/build_win64_aeryn.bat
@@ -1,4 +1,4 @@
-build --qt-gpl d:\tmp\qt-opensource --qt-commercial d:\tmp\qt-commercial --qt-eval d:\tmp\qt-eval --qt d:\tmp\qt-commercial %1 %2 %3 %4 %5 %6 %7 %8 %9
+call build --qt-gpl d:\tmp\qt-opensource --qt-commercial d:\tmp\qt-commercial --qt-eval d:\tmp\qt-eval --qt d:\tmp\qt-commercial %1 %2 %3 %4 %5 %6 %7 %8 %9
 scp qtjambi*eval* gunnar@anarki:~/public_html/packages/eval
 scp qtjambi*gpl* gunnar@anarki:~/public_html/packages/gpl
 scp qtjambi*commerc* gunnar@anarki:~/public_html/packages/commercial
