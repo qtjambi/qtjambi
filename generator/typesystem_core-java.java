aa90191976380819e776e90260c36db7571a2ffc
hfroilan <a@b>
2007-10-19 12:47:10 +0100

    remove __rcWidgetForObject from qsignalmapper, this has moved to qguisignalmapper.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 280720]

--- generator/typesystem_core-java.java
+++ generator/typesystem_core-java.java
@@ -565,6 +565,5 @@ class QBuffer___ extends QBuffer {
 class QSignalMapper___ extends QSignalMapper {
     
     private java.util.Hashtable<QObject, QObject> __rcObjectForObject = new java.util.Hashtable<QObject, QObject>();
-    private java.util.Hashtable<QObject, Object> __rcWidgetForObject = new java.util.Hashtable<QObject, Object>();
     
 }// class
