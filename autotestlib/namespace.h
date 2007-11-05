e3010a0284ad8df3ee1f9e20a9efa09ae04d4bc9
eblomfel <eblomfel@SpareRibs>
2007-11-05 14:27:55 +0100

    p4i integration Integrate 283017 from 4.3 to main: Test nested namespaces
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 283018]

--- autotestlib/namespace.h
+++ autotestlib/namespace.h
@@ -52,9 +52,71 @@ namespace NameSpace
         int x;
     };
 
+    namespace NameSpace2 
+    {
+
+        namespace NameSpace3 
+        {
+            class ObjectD;
+
+            class ObjectC: public ObjectA
+            {
+            public:
+                ObjectC(const QString &s) : m_fooBar(s) {}
+                ObjectC *fooBar(ObjectD *obj);
+
+                QString str() { return m_fooBar; }
+
+            private:
+                QString m_fooBar;
+            };
+
+            class InterfaceA
+            {
+            public:
+                virtual ObjectD *fooBar2(ObjectC *) = 0;
+                virtual InterfaceA *fooBar(InterfaceA *) = 0;
+            };
+
+            class ObjectD: public ObjectC, InterfaceA
+            {
+            public:
+                ObjectD(const QString &s) : ObjectC(s) {} 
+
+                ObjectD *fooBar2(ObjectC *obj)
+                {
+                    return new ObjectD(obj->str());
+                }
+
+                InterfaceA *fooBar(InterfaceA *obj)
+                {
+                    return obj->fooBar(fooBar(this));
+                }
+            };
+
+            class ValueB
+            {
+            public:
+                ValueB() : m_i(0) {}
+                ValueB(int i) : m_i(i) {}
+                ValueB(const ValueB &myValue) : m_i(myValue.m_i) {}
+               
+            private:
+                int m_i;
+            };
+        };
+
+    };
+
 };
 
+
+
 #ifndef QT_JAMBI_RUN
+inline NameSpace::NameSpace2::NameSpace3::ObjectC *NameSpace::NameSpace2::NameSpace3::ObjectC::fooBar(NameSpace::NameSpace2::NameSpace3::ObjectD *obj)
+{                
+    return new NameSpace::NameSpace2::NameSpace3::ObjectC(obj->str());
+}
 inline NameSpace::ObjectA *NameSpace::ObjectA::aFunc(NameSpace::ObjectA *a) { return a; }
 inline NameSpace::ObjectB *NameSpace::ObjectA::bFunc(NameSpace::ObjectB *b) { return b; }
 
--- com/trolltech/autotests/TestNamespace.java
+++ com/trolltech/autotests/TestNamespace.java
@@ -21,16 +21,32 @@ import org.junit.*;
 
 public class TestNamespace extends QApplicationTest {
 
-    @Test public void exists() {
-        try {
-            assertTrue(Class.forName("com.trolltech.autotests.generated.ObjectA") != null);
-            assertTrue(Class.forName("com.trolltech.autotests.generated.ObjectB") != null);
-            assertTrue(Class.forName("com.trolltech.autotests.generated.ValueA") != null);
-        } catch (Exception e) {
-            assertTrue(false);
-        }
+    @Test public void exists() throws ClassNotFoundException {
+        assertTrue(Class.forName("com.trolltech.autotests.generated.ObjectA") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.ObjectB") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.ValueA") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_InterfaceA") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_InterfaceAInterface") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_ObjectC") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_ObjectD") != null);
+        assertTrue(Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_ValueB") != null);
     }
+    
+    @Test public void testInheritance() throws ClassNotFoundException {
+        Class<?> clazzC = Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_ObjectC");
+        Class<?> clazzA = Class.forName("com.trolltech.autotests.generated.ObjectA");
+        assertEquals(clazzA, clazzC.getSuperclass());
 
+        Class<?> clazzD = Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_ObjectD");
+        assertEquals(clazzC, clazzD.getSuperclass());
+        
+        Class<?> interfaces[] = clazzD.getInterfaces();
+        assertEquals(1, interfaces.length);
+        
+        Class<?> interfaceA = Class.forName("com.trolltech.autotests.generated.NameSpace_NameSpace2_NameSpace3_InterfaceAInterface");
+        assertEquals(interfaceA, interfaces[0]);
+    }
+    
     @Test public void testCalls() {
         ObjectA a = new ObjectA();
         ObjectB b = new ObjectB();
@@ -43,6 +59,14 @@ public class TestNamespace extends QApplicationTest {
         assertEquals(a.aFuncPrefixed(a), a);
         assertEquals(a.bFuncPrefixed(b), b);
         assertEquals(a.vFuncPrefixed(v), v);
+        
+        {
+            NameSpace_NameSpace2_NameSpace3_ObjectD d = new NameSpace_NameSpace2_NameSpace3_ObjectD("fooBar");
+            NameSpace_NameSpace2_NameSpace3_ObjectC c = new NameSpace_NameSpace2_NameSpace3_ObjectC("barFoo");
+            
+            NameSpace_NameSpace2_NameSpace3_ObjectC temp = c.fooBar(d);
+            assertEquals("fooBar", temp.str());
+        }
    }
 
 }
