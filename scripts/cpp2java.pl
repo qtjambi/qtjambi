2769bd49e8e37eee9ac157105b9687be1de530d5
qt-user <qt-info@nokia.com>
2007-11-07 17:33:41 +0100

    p4i integration Integrate 283374 from 4.3 to main: Small script to ease the pain of porting cpp code to jambi.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 283380]

---
+++ scripts/cpp2java.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+import Tie;
+use Tie::File;
+
+# We eat input files indiscriminately
+die "cpp2java file1 [file2...fileN]\n" if @ARGV < 1;
+
+while ($file = shift @ARGV) {
+    tie @array, 'Tie::File', $file
+        or print "Failed to tie: " . $file . "\n" and next
+            if -e $file;
+
+    for (@array) {
+        s/->/\./g;
+        s/::/\./g;
+        s/\*//g;
+        s/foreach/for/g;
+        s/bool\s/boolean\s/g;
+        s/qreal\s/double\s/g;
+        s/&//g;
+        s/const\s//g;
+        s/QString\s/String\s/g;
+        s/QStringList/List<String>/g;
+    }
+}
