daed68a2e50aee8287b257a9f24f27cff8951fdc
eblomfel <eblomfel@SpareRibs>
2007-11-08 10:48:42 +0100

    Whitespace cleanup (tabs -> spaces)
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 283477]

--- qtjambi/qtjambidestructorevent.h
+++ qtjambi/qtjambidestructorevent.h
@@ -11,20 +11,20 @@ class QTJAMBI_EXPORT QtJambiDestructorEvent : public QEvent
 public:
     QtJambiDestructorEvent(void *pointer, int meta_type, int ownership, PtrDestructorFunction destructor_function)
 	: QEvent( QEvent::Type(513) ), 
-	m_pointer(pointer),
-	m_meta_type(meta_type),
-	m_ownership(ownership),
-	m_destructor_function(destructor_function)
+	  m_pointer(pointer),
+      m_meta_type(meta_type),
+	  m_ownership(ownership),
+	  m_destructor_function(destructor_function)
     { }
         
     inline void callDestructor() 
     {
-	if (m_pointer != 0 && m_meta_type != QMetaType::Void) {
-	    QMetaType::destroy(m_meta_type, m_pointer);
-	} else if (m_ownership == QtJambiLink::JavaOwnership && m_destructor_function) {
-	    m_destructor_function(m_pointer);
-	}
-	m_pointer = 0; 
+	    if (m_pointer != 0 && m_meta_type != QMetaType::Void) {
+	        QMetaType::destroy(m_meta_type, m_pointer);
+	    } else if (m_ownership == QtJambiLink::JavaOwnership && m_destructor_function) {
+	        m_destructor_function(m_pointer);
+	    }
+	    m_pointer = 0; 
     }
     
 private:
@@ -32,7 +32,6 @@ private:
     int m_meta_type;
     int m_ownership;
     PtrDestructorFunction m_destructor_function;
-
 };
 
 #endif
