01512b830109600e6b37bf90c35111b1392bceb3
qt-user <qt-info@nokia.com>
2007-11-12 13:23:14 +0100

    p4i integration Integrate 283898 from 4.3 to main: Doc: Edited class description of QNoNativeResourcesException
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 283899]

--- com/trolltech/qt/QNoNativeResourcesException.java
+++ com/trolltech/qt/QNoNativeResourcesException.java
@@ -15,23 +15,25 @@ package com.trolltech.qt;
 
 /**
  * The QNoNativeResourceException is used to indicate that the C++
- * part of an object is destroyed while it is still referenced in
- * Java.
- *
- * Most Qt Jambi objects consist of a C++ part and a Java peer. In
- * most cases one should rely completly on the Java memory model and
- * let objects be garbage collected, in which case this kind of
- * exception will never occur, as the garbage collector will only
- * collect unreferenced memory in Java. In some cases, the ownership
- * of an object is transferred to C++, such as QObjects that are
- * passed a parent or items added to a QGraphicsScene. In these cases
- * the C++ part of the object can be deleted explicitly, using for
- * instance the C++ "delete" keyword. When the C++ part of the object
- * has been deleted it can no longer be used and this exception is
- * thrown.
- *
- * It is possible to check if an object has a valid C++ part or not by
- * checking that the value of nativeId() is non-null.
+ * native resources of a Java object are destroyed while it is still
+ * referenced in Java.
+ * <p>
+ * Most Qt Jambi objects consist of a C++ part, i.e., native C++
+ * resources, and a Java peer that wraps the public functions of the
+ * native objects. In most cases one can rely on the Java memory
+ * model and let objects be garbage collected, in which case this
+ * kind of exception will never occur (the garbage collector will
+ * only collect unreferenced memory in Java).
+ * <p>
+ * In some cases, the ownership of an object is transferred to C++,
+ * for instance, when QObjects are passed a parent or items are added
+ * to a QGraphicsScene.  In these cases the C++ part of the object
+ * can be deleted explicitly by, for instance, using the C++ "delete"
+ * keyword. When the C++ part of the object has been deleted it can
+ * no longer be used and this exception is thrown.
+ * <p>
+ * It is possible to check if an object has a valid C++ part or not
+ * by checking that the value of nativeId() is non-null.
  */
 public class QNoNativeResourcesException extends RuntimeException {
     private static final long serialVersionUID = 1L;
