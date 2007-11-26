ce35f7dabf45be262b4614b96fb2c8218a9478b4
eblomfel <eblomfel@SpareRibs>
2007-11-26 14:38:27 +0100

    p4i integration Integrate 285743 from 4.3 to main: Add a test to verify that we are correctly replacing java objects that were created with the wrong meta object.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 285744]

---
+++ com/trolltech/autotests/TestConstruction.java
@@ -0,0 +1,30 @@
+package com.trolltech.autotests;
+
+import org.junit.Test;
+
+import com.trolltech.qt.core.QEvent;
+import com.trolltech.qt.core.QObject;
+import com.trolltech.qt.gui.QMdiArea;
+import com.trolltech.qt.gui.QMdiSubWindow;
+import com.trolltech.qt.gui.QWidget;
+
+import static org.junit.Assert.*;
+
+public class TestConstruction extends QApplicationTest {
+        
+    @Test
+    public void testQObjectConversionInConstructor() {
+        QMdiArea area = new QMdiArea() {
+            @Override
+            public boolean eventFilter(QObject o, QEvent e) {
+                return false;
+            }
+        };
+        area.show();
+        
+        QMdiSubWindow subWindow = area.addSubWindow(new QWidget());
+        
+        // Although it seems unlikely, this may actually be false, trust me.
+        assertTrue(subWindow instanceof QMdiSubWindow);
+    }
+}
