051db91b41ffcdaaa7acb074763fb1b00588a5d6
gunnar <gunnar@THAUMASIA>
2008-01-02 16:08:05 +0100

    Small example fix, when the mainwindow goes, the undo view should too..
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 290061]

--- com/trolltech/examples/UndoFramework.java
+++ com/trolltech/examples/UndoFramework.java
@@ -32,20 +32,20 @@ public class UndoFramework extends QMainWindow
     public UndoFramework()
     {
         undoStack = new QUndoStack();
-    
+
         createActions();
         createMenus();
-    
+
         undoStack.canRedoChanged.connect(redoAction, "setEnabled(boolean)");
         undoStack.canUndoChanged.connect(undoAction, "setEnabled(boolean)");
 
         createUndoView();
-    
+
         diagramScene = new DiagramScene();
         diagramScene.setSceneRect(new QRectF(0, 0, 500, 500));
-    
+
         diagramScene.itemMoved.connect(this, "itemMoved(UndoFramework$DiagramItem,QPointF)");
-    
+
         setWindowTitle("Undo Framework");
         QGraphicsView view = new QGraphicsView(diagramScene);
         setCentralWidget(view);
@@ -56,8 +56,13 @@ public class UndoFramework extends QMainWindow
     {
         undoView = new QUndoView(undoStack);
         undoView.setWindowTitle(tr("Command List"));
-        undoView.show();
         undoView.setAttribute(Qt.WidgetAttribute.WA_QuitOnClose, false);
+
+        QDialog dialog = new QDialog(this);
+        QVBoxLayout layout = new QVBoxLayout(dialog);
+        layout.setContentsMargins(0, 0, 0, 0);
+        layout.addWidget(undoView);
+        dialog.show();
     }
 
     private void createActions()
@@ -65,20 +70,20 @@ public class UndoFramework extends QMainWindow
         deleteAction = new QAction(tr("&Delete Item"), this);
         deleteAction.setShortcut(tr("Del"));
         deleteAction.triggered.connect(this, "deleteItem()");
-    
+
         addBoxAction = new QAction(tr("Add &Box"), this);
         addBoxAction.setShortcut(tr("Ctrl+O"));
         addBoxAction.triggered.connect(this, "addBox()");
-    
+
         addTriangleAction = new QAction(tr("Add &Triangle"), this);
         addTriangleAction.setShortcut(tr("Ctrl+T"));
         addTriangleAction.triggered.connect(this, "addTriangle()");
-    
+
         undoAction = new QAction(tr("&Undo"), this);
         undoAction.setShortcut(tr("Ctrl+Z"));
         undoAction.setEnabled(false);
         undoAction.triggered.connect(undoStack, "undo()");
-    
+
         redoAction = new QAction(tr("&Redo"), this);
         List<QKeySequence> redoShortcuts = new LinkedList<QKeySequence>();
         redoShortcuts.add(new QKeySequence(tr("Ctrl+Y")));
@@ -86,11 +91,11 @@ public class UndoFramework extends QMainWindow
         redoAction.setShortcuts(redoShortcuts);
         redoAction.setEnabled(false);
         redoAction.triggered.connect(undoStack, "redo()");
-    
+
         exitAction = new QAction(tr("E&xit"), this);
         exitAction.setShortcut(tr("Ctrl+Q"));
         exitAction.triggered.connect(this, "close()");
-    
+
         aboutAction = new QAction(tr("&About"), this);
         List<QKeySequence> aboutShortcuts = new LinkedList<QKeySequence>();
         aboutShortcuts.add(new QKeySequence(tr("Ctrl+A")));
@@ -103,7 +108,7 @@ public class UndoFramework extends QMainWindow
     {
         fileMenu = menuBar().addMenu(tr("&File"));
         fileMenu.addAction(exitAction);
-    
+
         editMenu = menuBar().addMenu(tr("&Edit"));
         editMenu.addAction(undoAction);
         editMenu.addAction(redoAction);
@@ -115,7 +120,7 @@ public class UndoFramework extends QMainWindow
         itemMenu = menuBar().addMenu(tr("&Item"));
         itemMenu.addAction(addBoxAction);
         itemMenu.addAction(addTriangleAction);
-    
+
         helpMenu = menuBar().addMenu(tr("&About"));
         helpMenu.addAction(aboutAction);
     }
@@ -209,7 +214,7 @@ public class UndoFramework extends QMainWindow
         public DiagramType diagramType() {
             return polygon().subtracted(boxPolygon).isEmpty() ? DiagramType.Box : DiagramType.Triangle;
         }
-    } 
+    }
 
     class DiagramScene extends QGraphicsScene
     {
@@ -255,7 +260,7 @@ public class UndoFramework extends QMainWindow
             double startY = rect.top() - Math.IEEEremainder(rect.top(), 30.0);
             for (; startY < rect.bottom(); startY += 30.0)
                 painter.drawLine(new QPointF(rect.left(), startY),
-                                 new QPointF(rect.right(), startY)); 
+                                 new QPointF(rect.right(), startY));
 
             double startX = rect.left() - Math.IEEEremainder(rect.left(), 30.0);
             for (; startX < rect.right(); startX += 30.0)
@@ -315,27 +320,27 @@ public class UndoFramework extends QMainWindow
             myDiagramItem.setPos(myOldPos);
             myDiagramItem.scene().update();
             setText(tr("Move " + UndoFramework.createCommandString(myDiagramItem, newPos)));
-       } 
+       }
 
         @Override
         public void redo()
         {
             myDiagramItem.setPos(newPos);
             setText(tr("Move " + UndoFramework.createCommandString(myDiagramItem, newPos)));
-        }    
+        }
 
         @Override
         public boolean mergeWith(QUndoCommand other)
         {
             MoveCommand moveCommand = (MoveCommand) other;
             DiagramItem item = moveCommand.myDiagramItem;
-        
+
             if (!myDiagramItem.equals(item))
                 return false;
-        
+
             newPos = item.pos();
             setText(tr("Move " + UndoFramework.createCommandString(myDiagramItem, newPos)));
-        
+
             return true;
         }
     }
@@ -375,7 +380,7 @@ public class UndoFramework extends QMainWindow
     }
 
     public static String createCommandString(DiagramItem item, QPointF pos)
-    {        
+    {
         return QCoreApplication.translate("DiagramItem", (item.diagramType() == DiagramType.Box ? "Box" : "Triangle") +
                " at (" + pos.x() + ", " + pos.y() + ")");
     }
