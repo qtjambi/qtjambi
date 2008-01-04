bcd664e3aa0dda00a5220ab3601b4e24f83ba781
hfroilan <a@b>
2008-01-04 13:20:32 +0100

    Javacode injected into generated code by typesystem. (for webkit)
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 290320]

---
+++ generator/typesystem_webkit-java.java
@@ -0,0 +1,16 @@
+package generator;
+
+import com.trolltech.qt.*;
+import com.trolltech.qt.webkit.*;
+
+class QWebPage___ extends QWebPage {
+    protected String javaScriptPrompt(com.trolltech.qt.webkit.QWebFrame originatingFrame, java.lang.String msg, java.lang.String defaultValue) {
+        com.trolltech.qt.QNativePointer result = new com.trolltech.qt.QNativePointer(QNativePointer.Type.String);
+        if (javaScriptPrompt(originatingFrame, msg, defaultValue, result))
+            return result.stringValue();
+        else
+            return null;        
+    }
+
+}// class
+
