aeadc97a355bf1696885ce3bd021f9c2f3f2d38c
gunnar <gunnar@THAUMASIA>
2008-01-10 13:17:25 +0100

    a demo web browser...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 291143]

--- com/trolltech/demos/webkit/HelloWebKit.java
+++ com/trolltech/demos/webkit/HelloWebKit.java
@@ -4,40 +4,56 @@ import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 import com.trolltech.qt.webkit.*;
 
-class HelloWebKit extends QWidget {
+class HelloWebKit extends QMainWindow {
 
     private QWebView browser;
     private QLineEdit field;
-    private QLabel state;
+
+    private QAction forward;
+    private QAction backward;
+    private QAction reload;
+    private QAction stop;
 
     public HelloWebKit() {
         field = new QLineEdit();
         browser = new QWebView();
-        state = new QLabel("No page loaded...");
-        state.setFixedWidth(200);
 
-        QGridLayout layout = new QGridLayout(this);
-        layout.addWidget(field, 0, 0);
-        layout.addWidget(state, 0, 1);
-        layout.addWidget(browser, 1, 0, 1, 2);
+        // Toolbar...
+        QToolBar toolbar = addToolBar("Actions");
+        backward = toolbar.addAction("Backward");
+        forward = toolbar.addAction("Forward");
+        reload = toolbar.addAction("Reload");
+        stop = toolbar.addAction("Stop");
+        toolbar.addWidget(field);
+        toolbar.setFloatable(false);
+        toolbar.setMovable(false);
 
+        setCentralWidget(browser);
+        statusBar().show();
+
+        // Connections
         field.returnPressed.connect(this, "open()");
 
         browser.loadStarted.connect(this, "loadStarted()");
         browser.loadProgressChanged.connect(this, "loadProgress(int)");
         browser.loadFinished.connect(this, "loadDone()");
+
+        forward.triggered.connect(browser, "forward()");
+        backward.triggered.connect(browser, "backward()");
+        reload.triggered.connect(browser, "reload()");
+        stop.triggered.connect(browser, "stop()");
     }
 
     public void loadStarted() {
-        state.setText("Starting to load: " + field.text());
+        statusBar().showMessage("Starting to load: " + field.text());
     }
 
     public void loadDone() {
-        state.setText("Loading done...");
+        statusBar().showMessage("Loading done...");
     }
 
     public void loadProgress(int x) {
-        state.setText("Loading: " + x + " %");
+        statusBar().showMessage("Loading: " + x + " %");
     }
 
     public void open() {
