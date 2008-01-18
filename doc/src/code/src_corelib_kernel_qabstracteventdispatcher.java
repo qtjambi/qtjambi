0ff3067e3e7e0c5a4da9580744f9101d7c01cc2a
qt-user <qt-info@nokia.com>
2008-01-18 15:14:08 +0100

    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292736]

---
+++ doc/src/code/src_corelib_kernel_qabstracteventdispatcher.java
@@ -0,0 +1,31 @@
+/*   Ported from: src.corelib.kernel.qabstracteventdispatcher.cpp
+<snip>
+//! [0]
+        bool myEventFilter(void *message);
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_corelib_kernel_qabstracteventdispatcher {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+/*
+ * This has been commented out because it is not part of the QtJambi api
+ * 
+//! [0]
+        boolean myEventFilter(void message);
+//! [0]
+*/
+
+    }
+}
