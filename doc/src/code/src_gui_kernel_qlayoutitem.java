2f8ef2217a072e87979c7c2d2f0704b8b2dd0dad
qt-user <qt-info@nokia.com>
2008-01-18 15:45:42 +0100

    ported C++ to Java
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292786]

--- doc/src/code/src_gui_kernel_qlayoutitem.java
+++ doc/src/code/src_gui_kernel_qlayoutitem.java
@@ -1,22 +1,3 @@
-/*   Ported from: src.gui.kernel.qlayoutitem.cpp
-<snip>
-//! [0]
-        int MyLayout::heightForWidth(int w) const
-        {
-            if (cache_dirty || cached_width != w) {
-                // not all C++ compilers support "mutable"
-                MyLayout *that = (MyLayout*)this;
-                int h = calculateHeightForWidth(w);
-                that->cached_hfw = h;
-                return h;
-            }
-            return cached_hfw;
-        }
-//! [0]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -26,23 +7,23 @@ import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
 
 
-public class src_gui_kernel_qlayoutitem {
-    public static void main(String args[]) {
-        QApplication.initialize(args);
+abstract class MyLayout extends QLayoutItem {
+
+    int cached_width = 1;
+    int cached_hfw = 1;
+    boolean cache_dirty = true;
+
+    private int calculateHeightForWidth(int w) { return 1; }
+
 //! [0]
-        int MyLayout.heightForWidth(int w)
-        {
-            if (cache_dirty || cached_width != w) {
-                // not all C++ compilers support "mutable"
-                MyLayout hat = (MyLayout*)this;
-                int h = calculateHeightForWidth(w);
-                that.cached_hfw = h;
-                return h;
-            }
-            return cached_hfw;
+    public int heightForWidth(int w)
+    {
+        if (cache_dirty || cached_width != w) {
+            int h = calculateHeightForWidth(w);
+            cached_hfw = h;
+            return h;
         }
-//! [0]
-
-
+        return cached_hfw;
     }
+//! [0]
 }
