e5dec30c673821af608b42075fb5239410256119
Jens Bache-Wiig <jbache@trolltech.com>
2008-01-18 12:58:05 +0100

    fix for qfontmetrics
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292562]

--- doc/src/code/src_gui_text_qfontmetrics.java
+++ doc/src/code/src_gui_text_qfontmetrics.java
@@ -1,22 +1,4 @@
 /*   Ported from: src.gui.text.qfontmetrics.cpp
-<snip>
-//! [0]
-    QFont font("times", 24);
-    QFontMetrics fm(font);
-    int pixelsWide = fm.width("What's the width of this text?");
-    int pixelsHigh = fm.height();
-//! [0]
-
-
-//! [1]
-    QFont font("times", 24);
-    QFontMetricsF fm(font);
-    qreal pixelsWide = fm.width("What's the width of this text?");
-    qreal pixelsHigh = fm.height();
-//! [1]
-
-
-</snip>
 */
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
@@ -30,21 +12,23 @@ import com.trolltech.qt.svg.*;
 public class src_gui_text_qfontmetrics {
     public static void main(String args[]) {
         QApplication.initialize(args);
+{
 //! [0]
-    QFont font("times", 24);
-    QFontMetrics fm(font);
+    QFont font = new QFont("times", 24);
+    QFontMetrics fm = new QFontMetrics(font);
     int pixelsWide = fm.width("What's the width of this text?");
     int pixelsHigh = fm.height();
 //! [0]
+}
 
-
+{
 //! [1]
-    QFont font("times", 24);
-    QFontMetricsF fm(font);
+    QFont font = new QFont("times", 24);
+    QFontMetricsF fm = new QFontMetricsF(font);
     double pixelsWide = fm.width("What's the width of this text?");
     double pixelsHigh = fm.height();
 //! [1]
-
+}
 
     }
 }
