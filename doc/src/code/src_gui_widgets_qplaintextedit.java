363bf3b37a7ede6c73e31e7f2ed09f3b723214e3
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:51:53 +0100

    Would compile if QPlainTextEdit is in QJambi
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292548]

---
+++ doc/src/code/src_gui_widgets_qplaintextedit.java
@@ -0,0 +1,53 @@
+/*   Ported from: src.gui.widgets.qplaintextedit.cpp
+<snip>
+//! [0]
+  void MyQPlainTextEdit::contextMenuEvent(QContextMenuEvent *event)
+  {
+      QMenu *menu = createStandardContextMenu();
+      menu->addAction(tr("My Menu Item"));
+      //...
+      menu->exec(event->globalPos());
+      delete menu;
+  }
+//! [0]
+
+
+//! [1]
+    edit->textCursor().insertText(text);
+//! [1]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qplaintextedit extends QPlainTextEdit {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+    }
+//! [0]
+  protected void contextMenuEvent(QContextMenuEvent event)
+  {
+      QMenu menu = createStandardContextMenu();
+      menu.addAction(tr("My Menu Item"));
+      //...
+      menu.exec(event.globalPos());
+  }
+//! [0]
+
+
+  private void foo(QPlainTextEdit edit, String text) {
+//! [1]
+    edit.textCursor().insertText(text);
+//! [1]
+
+
+    }
+}
