70bc52d976ec11833a062184061fbf933c5f237c
qt-user <qt-info@nokia.com>
2008-01-18 15:04:11 +0100

    <ported>
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292723]

--- doc/src/code/src_gui_text_qtextlayout.java
+++ doc/src/code/src_gui_text_qtextlayout.java
@@ -1,33 +1,3 @@
-/*   Ported from: src.gui.text.qtextlayout.cpp
-<snip>
-//! [0]
-        int leading = fontMetrics.leading();
-        qreal height = 0;
-        qreal widthUsed = 0;
-        textLayout.beginLayout();
-        while (1) {
-            QTextLine line = textLayout.createLine();
-            if (!line.isValid())
-                break;
-
-            line.setLineWidth(lineWidth);
-            height += leading;
-            line.setPosition(QPointF(0, height));
-            height += line.height();
-            widthUsed = qMax(widthUsed, line.naturalTextWidth());
-        }
-        textLayout.endLayout();
-//! [0]
-
-
-//! [1]
-        QPainter painter(this);
-        textLayout.draw(&painter, QPoint(0, 0));
-//! [1]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -35,34 +5,42 @@ import com.trolltech.qt.xml.*;
 import com.trolltech.qt.network.*;
 import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
+import java.lang.Math;
+import java.util.*;
 
 
 public class src_gui_text_qtextlayout {
     public static void main(String args[]) {
         QApplication.initialize(args);
+
+
+QFontMetrics fontMetrics=new QFontMetrics(new QFont());
+QTextLayout textLayout=new QTextLayout();
+int lineWidth=3;
+
 //! [0]
         int leading = fontMetrics.leading();
         double height = 0;
         double widthUsed = 0;
         textLayout.beginLayout();
-        while (1) {
+        while (1==1) {
             QTextLine line = textLayout.createLine();
             if (!line.isValid())
                 break;
 
             line.setLineWidth(lineWidth);
             height += leading;
-            line.setPosition(QPointF(0, height));
+            line.setPosition(new QPointF(0.0, height));
             height += line.height();
-            widthUsed = qMax(widthUsed, line.naturalTextWidth());
+            widthUsed = Math.max(widthUsed, line.naturalTextWidth());
         }
         textLayout.endLayout();
 //! [0]
 
 
 //! [1]
-        QPainter painter(this);
-        textLayout.draw(ainter, QPoint(0, 0));
+        QPainter painter = new QPainter();
+        textLayout.draw(painter,new QPointF(), new ArrayList<QTextLayout_FormatRange>());
 //! [1]
 
 
