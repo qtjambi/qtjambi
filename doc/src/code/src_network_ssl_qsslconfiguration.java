3f233adf2744836704e9e73a49d9cf602e73a053
Thorbjørn Lindeijer <tlindeij@trolltech.com>
2008-01-18 16:12:30 +0100

    Ported src_network_ssl_qsslconfiguration.java (but the necessary classes are not available in Qt Jambi)
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292803]

---
+++ doc/src/code/src_network_ssl_qsslconfiguration.java
@@ -0,0 +1,22 @@
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_network_ssl_qsslconfiguration {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+/*
+        QSslSocket sslSocket = new QSslSocket();
+//! [0]
+        QSslConfiguration config = sslSocket.sslConfiguration();
+        config.setProtocol(QSsl.TlsV1);
+        sslSocket.setSslConfiguration(config);
+//! [0]
+*/
+    }
+}
