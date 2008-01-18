5cba2ac1f51f733ed413cebe3dc761371b7db4c6
Thorbjørn Lindeijer <tlindeij@trolltech.com>
2008-01-18 11:30:38 +0100

    There is no MyGLWidget, but otherwise it compiles...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292454]

---
+++ doc/src/code/src_opengl_qglcolormap.java
@@ -0,0 +1,23 @@
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QColor;
+import com.trolltech.qt.opengl.QGLColormap;
+import com.trolltech.qt.opengl.QGLWidget;
+
+public class src_opengl_qglcolormap {
+//! [0]
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+
+        MyGLWidget widget = new MyGLWidget();  // a QGLWidget in color-index mode
+        QGLColormap colormap = new QGLColormap();
+
+        // This will fill the colormap with colors ranging from black to white.
+        for (int i = 0; i < colormap.size(); i++)
+            colormap.setEntry(i, new QColor(i, i, i).rgb());
+
+        widget.setColormap(colormap);
+        widget.show();
+        QApplication.exec();
+    }
+//! [0]
+}
