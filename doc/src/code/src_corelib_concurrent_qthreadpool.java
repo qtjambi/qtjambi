7ba927b4da27a733f15fa6ae07dccadfce2695d4
eblomfel <eblomfel@SpareRibs>
2008-01-18 14:25:49 +0100

    QThreadPool
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292690]

---
+++ doc/src/code/src_corelib_concurrent_qthreadpool.java
@@ -0,0 +1,23 @@
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+
+
+public class src_corelib_concurrent_qthreadpool {
+//! [0]
+        static class HelloWorldTask extends QRunnable {
+            @Override
+            public void run()
+            {
+                System.out.println("Hello world from thread" + QThread.currentThread());
+            }
+        }
+
+        public static void main(String args[]) {
+            QCoreApplication.initialize(args);
+        
+            HelloWorldTask hello = new HelloWorldTask();
+            // QThreadPool takes ownership and deletes 'hello' automatically
+            QThreadPool.globalInstance().start(hello);
+        }
+//! [0]
+}
