82a6b34dbd185bcc0770ec43f3ea26cbc1a63ec2
Thomas Cooksey <thomas.cooksey@trolltech.com>
2008-01-18 14:25:42 +0100

    Adapted snipped to java for Snippy Party Day
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292689]

---
+++ doc/src/code/src_corelib_io_qtemporaryfile.java
@@ -0,0 +1,42 @@
+/*   Ported from: src.corelib.io.qtemporaryfile.cpp
+<snip>
+//! [0]
+        {
+            QTemporaryFile file;
+            if (file.open()) {
+                // file.fileName() returns the unique file name
+            }
+
+            // the QTemporaryFile destructor removes the temporary file
+        }
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_corelib_io_qtemporaryfile {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+//! [0]
+        {
+            QTemporaryFile file = new QTemporaryFile();
+            if (file.open()) {
+                // file.fileName() returns the unique file name
+            }
+
+            // the QTemporaryFile destructor removes the temporary file
+        }
+//! [0]
+
+
+    }
+}
