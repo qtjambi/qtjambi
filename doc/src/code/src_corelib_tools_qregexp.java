8cf491123574116815c070ed94ff97f5c765b655
qt-user <qt-info@nokia.com>
2008-01-18 16:24:15 +0100

    Ported QtJambi snippets.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292811]

--- doc/src/code/src_corelib_tools_qregexp.java
+++ doc/src/code/src_corelib_tools_qregexp.java
@@ -194,87 +194,106 @@ import com.trolltech.qt.xml.*;
 import com.trolltech.qt.network.*;
 import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
+import java.util.*;
 
 
 public class src_corelib_tools_qregexp {
     public static void main(String args[]) {
         QApplication.initialize(args);
+    }
+    public void foo1()
+    {
 //! [0]
-        QRegExp rx("(\\d+)");
-        Stringsstr = "Offsets: 12 14 99 231 7";
-        List<String> list;
+        QRegExp rx = new QRegExp("(\\d+)");
+        String str = "Offsets: 12 14 99 231 7";
+        LinkedList<String> list = new LinkedList<String>();
         int pos = 0;
 
         while ((pos = rx.indexIn(str, pos)) != -1) {
-            list << rx.cap(1);
+            list.add(rx.cap(1));
             pos += rx.matchedLength();
         }
         // list: ["12", "14", "99", "231", "7"]
 //! [0]
+    }
 
-
+    public void foo2()
+    {
 //! [1]
-        QRegExp rx("*.txt");
-        rx.setPatternSyntax(QRegExp.Wildcard);
+        QRegExp rx = new QRegExp("*.txt");
+        rx.setPatternSyntax(QRegExp.PatternSyntax.Wildcard);
         rx.exactMatch("README.txt");        // returns true
         rx.exactMatch("welcome.txt.bak");   // returns false
 //! [1]
+    }
 
 
 //! [2]
- QRegExp
+// QRegExp
 //! [2]
 
 
+    public void foo3()
+    {
 //! [3]
-        QRegExp mark("\\b"      // word boundary
-                      "[Mm]ark" // the word we want to match
-                    );
+        QRegExp mark = new QRegExp("\\b"      // word boundary
+				 + "[Mm]ark"  // the word we want to match
+				   );
 //! [3]
+    }
 
-
+    public void foo4()
+    {
 //! [4]
-        QRegExp rx("^\\d\\d?$");    // match integers 0 to 99
-        rx.indexIn("123");          // returns -1 (no match)
-        rx.indexIn("-6");           // returns -1 (no match)
-        rx.indexIn("6");            // returns 0 (matched as position 0)
+        QRegExp rx = new QRegExp("^\\d\\d?$");  // match integers 0 to 99
+        rx.indexIn("123");                      // returns -1 (no match)
+        rx.indexIn("-6");                       // returns -1 (no match)
+        rx.indexIn("6");                        // returns 0 (matched as position 0)
 //! [4]
+    }
 
-
+    public void foo5()
+    {
 //! [5]
-        QRegExp rx("^\\S+$");       // match strings without whitespace
-        rx.indexIn("Hello world");  // returns -1 (no match)
-        rx.indexIn("This_is-OK");   // returns 0 (matched at position 0)
+        QRegExp rx = new QRegExp("^\\S+$"); // match strings without whitespace
+        rx.indexIn("Hello world");          // returns -1 (no match)
+        rx.indexIn("This_is-OK");           // returns 0 (matched at position 0)
 //! [5]
+    }
 
-
+    public void foo6()
+    {
 //! [6]
-        QRegExp rx("\\b(mail|letter|correspondence)\\b");
+        QRegExp rx = new QRegExp("\\b(mail|letter|correspondence)\\b");
         rx.indexIn("I sent you an email");     // returns -1 (no match)
         rx.indexIn("Please write the letter"); // returns 17
 //! [6]
 
 
 //! [7]
-        Stringscaptured = rx.cap(1); // captured == "letter"
+        String captured = rx.cap(1); // captured == "letter"
 //! [7]
+    }
 
-
+    public void foo7()
+    {
 //! [8]
-        QRegExp rx("&(?!amp;)");      // match ampersands but not mp;
-        Stringsline1 = "This & that";
+        /*QRegExp rx = new QRegExp("&(?!amp;)"); // match ampersands but not mp;
+        String line1 = "This & that";
         line1.replace(rx, "mp;");
         // line1 == "This mp; that"
-        Stringsline2 = "His mp; hers & theirs";
+        String line2 = "His mp; hers & theirs";
         line2.replace(rx, "mp;");
-        // line2 == "His mp; hers mp; theirs"
+        // line2 == "His mp; hers mp; theirs"*/
 //! [8]
+    }
 
-
+    public void foo8()
+    {
 //! [9]
-        Stringsstr = "One Eric another Eirik, and an Ericsson. "
-                      "How many Eiriks, Eric?";
-        QRegExp rx("\\b(Eric|Eirik)\\b"); // match Eric or Eirik
+        String str = "One Eric another Eirik, and an Ericsson. "
+	           + "How many Eiriks, Eric?";
+        QRegExp rx = new QRegExp("\\b(Eric|Eirik)\\b"); // match Eric or Eirik
         int pos = 0;    // where we are in the string
         int count = 0;  // how many Eric and Eirik's we've counted
         while (pos >= 0) {
@@ -289,7 +308,7 @@ public class src_corelib_tools_qregexp {
 
 //! [10]
         str = "$TROLLTECH$\twww.trolltech.com\tNorway";
-        Stringscompany, web, country;
+        String company, web, country;
         rx.setPattern("^([^\t]+)\t([^\t]+)\t([^\t]+)$");
         if (rx.indexIn(str) != -1) {
             company = rx.cap(1);
@@ -300,22 +319,26 @@ public class src_corelib_tools_qregexp {
 
 
 //! [11]
-        List<String> field = str.split("\t");
+        String[] field = str.split("\t");
 //! [11]
+    }
 
-
+    public void foo9()
+    {
 //! [12]
-        QRegExp rx("*.html");
-        rx.setPatternSyntax(QRegExp.Wildcard);
+        QRegExp rx = new QRegExp("*.html");
+        rx.setPatternSyntax(QRegExp.PatternSyntax.Wildcard);
         rx.exactMatch("index.html");                // returns true
         rx.exactMatch("default.htm");               // returns false
         rx.exactMatch("readme.txt");                // returns false
 //! [12]
+    }
 
-
+    public void foo10()
+    {
 //! [13]
-        Stringsstr = "offsets: 1.23 .50 71.00 6.00";
-        QRegExp rx("\\d*\\.\\d+");    // primitive floating point matching
+        String str = "offsets: 1.23 .50 71.00 6.00";
+        QRegExp rx = new QRegExp("\\d*\\.\\d+"); // primitive floating point matching
         int count = 0;
         int pos = 0;
         while ((pos = rx.indexIn(str, pos)) != -1) {
@@ -324,65 +347,87 @@ public class src_corelib_tools_qregexp {
         }
         // pos will be 9, 14, 18 and finally 24; count will end up as 4
 //! [13]
+    }
 
 
+    public void foo11()
+    {
 //! [14]
-        QRegExp rx("(\\d+)(\\s*)(cm|inch(es)?)");
+        QRegExp rx = new QRegExp("(\\d+)(\\s*)(cm|inch(es)?)");
         int pos = rx.indexIn("Length: 36 inches");
         List<String> list = rx.capturedTexts();
         // list is now ("36 inches", "36", " ", "inches", "es")
 //! [14]
+    }
 
 
+    public void foo12()
+    {
 //! [15]
-        QRegExp rx("(\\d+)(?:\\s*)(cm|inch(?:es)?)");
+        QRegExp rx = new QRegExp("(\\d+)(?:\\s*)(cm|inch(?:es)?)");
         int pos = rx.indexIn("Length: 36 inches");
         List<String> list = rx.capturedTexts();
         // list is now ("36 inches", "36", "inches")
 //! [15]
+    }
 
-
+    public void foo12b()
+    {
+        QRegExp rx = new QRegExp("(\\d+)(?:\\s*)(cm|inch(?:es)?)");
 //! [16]
         List<String> list = rx.capturedTexts();
-        List<String>.iterator it = list.begin();
-        while (it != list.end()) {
-            myProcessing(t);
-            ++it;
+        ListIterator<String> it = list.listIterator();
+        while (it.hasNext()) {
+	    String t = it.next();
+            //myProcessing();
         }
 //! [16]
+    }
 
 
+    public void foo13()
+    {
 //! [17]
-        QRegExp rxlen("(\\d+)(?:\\s*)(cm|inch)");
+        QRegExp rxlen = new QRegExp("(\\d+)(?:\\s*)(cm|inch)");
         int pos = rxlen.indexIn("Length: 189cm");
         if (pos > -1) {
-            Stringsvalue = rxlen.cap(1); // "189"
-            Stringsunit = rxlen.cap(2);  // "cm"
+            String value = rxlen.cap(1); // "189"
+            String unit = rxlen.cap(2);  // "cm"
             // ...
         }
 //! [17]
+    }
 
 
+    public void foo14()
+    {
 //! [18]
-        QRegExp rx("/([a-z]+)/([a-z]+)");
+        QRegExp rx = new QRegExp("/([a-z]+)/([a-z]+)");
         rx.indexIn("Output /dev/null");   // returns 7 (position of /dev/null)
         rx.pos(0);                        // returns 7 (position of /dev/null)
         rx.pos(1);                        // returns 8 (position of dev)
         rx.pos(2);                        // returns 12 (position of null)
 //! [18]
+    }
 
 
+    public void foo15()
+    {
+	String s1;
+	String s2;
 //! [19]
         s1 = QRegExp.escape("bingo");   // s1 == "bingo"
         s2 = QRegExp.escape("f(x)");    // s2 == "f\\(x\\)"
 //! [19]
+    }
 
-
+    public void foo16()
+    {
+	String name = new String();
+	String alias = new String();
 //! [20]
-        QRegExp rx("(" + QRegExp.escape(name) +
-                   "|" + QRegExp.escape(alias) + ")");
+        QRegExp rx = new QRegExp("(" + QRegExp.escape(name) +
+				 "|" + QRegExp.escape(alias) + ")");
 //! [20]
-
-
     }
 }
