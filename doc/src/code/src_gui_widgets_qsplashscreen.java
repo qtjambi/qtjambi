e7b9b04cdbba342db67b582fa08b441aaf781ff7
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 13:39:44 +0100

    Compiles now
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292609]

---
+++ doc/src/code/src_gui_widgets_qsplashscreen.java
@@ -0,0 +1,52 @@
+/*   Ported from: src.gui.widgets.qsplashscreen.cpp
+<snip>
+//! [0]
+       QPixmap pixmap(":/splash.png");
+       QSplashScreen *splash = new QSplashScreen(pixmap);
+       splash->show();
+
+       ... // Loading some items
+       splash->showMessage("Loaded modules");
+
+       qApp->processEvents();
+
+       ... // Establishing connections
+       splash->showMessage("Established connections");
+
+       qApp->processEvents();
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qsplashscreen {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+//! [0]
+       QPixmap pixmap = new QPixmap(":/splash.png");
+       QSplashScreen splash = new QSplashScreen(pixmap);
+       splash.show();
+
+       // Loading some items ...
+       splash.showMessage("Loaded modules");
+
+       QApplication.processEvents();
+
+       // Establishing connections ...
+       splash.showMessage("Established connections");
+
+       QApplication.processEvents();
+//! [0]
+
+
+    }
+}
