a8cc03bc4cf7bfe849216569249f83caef5e01ff
Trond Kjernaasen <trond@trolltech.com>
2008-01-18 15:46:13 +0100

    port!
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292788]

--- doc/src/code/src_sql_kernel_qsqlresult.java
+++ doc/src/code/src_sql_kernel_qsqlresult.java
@@ -1,54 +1,5 @@
-/*   Ported from: src.sql.kernel.qsqlresult.cpp
-<snip>
-//! [0]
-    QSqlQuery q;
-    q.prepare("insert into test (i1, i2, s) values (?, ?, ?)");
+import java.util.*;
 
-    QVariantList col1;
-    QVariantList col2;
-    QVariantList col3;
-
-    col1 << 1 << 3;
-    col2 << 2 << 4;
-    col3 << "hello" << "world";
-
-    q.bindValue(0, col1);
-    q.bindValue(1, col2);
-    q.bindValue(2, col3);
-
-    if (!q.execBatch())
-        qDebug() << q.lastError();
-//! [0]
-
-
-//! [1]
-    QSqlQuery query = ...
-    QVariant v = query.result()->handle();
-    if (v.isValid() && qstrcmp(v.typeName(), "sqlite3_stmt*")) {
-        // v.data() returns a pointer to the handle
-        sqlite3_stmt *handle = *static_cast<sqlite3_stmt **>(v.data());
-        if (handle != 0) { // check that it is not NULL
-            ...
-        }
-    }
-//! [1]
-
-
-//! [2]
-    if (v.typeName() == "PGresult*") {
-        PGresult *handle = *static_cast<PGresult **>(v.data());
-        if (handle != 0) ...
-    }
-
-    if (v.typeName() == "MYSQL_STMT*") {
-        MYSQL_STMT *handle = *static_cast<MYSQL_STMT **>(v.data());
-        if (handle != 0) ...
-    }
-//! [2]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -62,28 +13,31 @@ public class src_sql_kernel_qsqlresult {
     public static void main(String args[]) {
         QApplication.initialize(args);
 //! [0]
-    QSqlQuery q;
+    QSqlQuery q = new QSqlQuery();
     q.prepare("insert into test (i1, i2, s) values (?, ?, ?)");
 
-    QVariantList col1;
-    QVariantList col2;
-    QVariantList col3;
+    List<Integer> col1 = new ArrayList<Integer>();
+    List<Integer> col2 = new ArrayList<Integer>();
+    List<String>  col3 = new ArrayList<String>();
 
-    col1 << 1 << 3;
-    col2 << 2 << 4;
-    col3 << "hello" << "world";
+    col1.add(1);
+    col1.add(3);
+    col2.add(2);
+    col2.add(4);
+    col3.add("hello");
+    col3.add("world");
 
     q.bindValue(0, col1);
     q.bindValue(1, col2);
     q.bindValue(2, col3);
 
     if (!q.execBatch())
-        qDebug() << q.lastError();
+        System.out.println(q.lastError());
 //! [0]
 
-
+/* Not relevant in Java
 //! [1]
-    QSqlQuery query = ...
+    QSqlQuery query = ...;
     QVariant v = query.result().handle();
     if (v.isValid() && qstrcmp(v.typeName(), "sqlite3_stmt*")) {
         // v.data() returns a pointer to the handle
@@ -106,7 +60,7 @@ public class src_sql_kernel_qsqlresult {
         if (handle != 0) ...
     }
 //! [2]
-
+*/
 
     }
 }
