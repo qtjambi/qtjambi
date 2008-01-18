10a46536b2a6a57c2a808cdf0d1019e903a88130
qt-user <qt-info@nokia.com>
2008-01-18 12:47:27 +0100

    port snippet to java
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292547]

---
+++ doc/src/code/src_corelib_tools_qrect.java
@@ -0,0 +1,44 @@
+/*   Ported from: src.corelib.tools.qrect.cpp
+<snip>
+//! [0]
+        QRect r1(100, 200, 11, 16);
+        QRect r2(QPoint(100, 200), QSize(11, 16));
+//! [0]
+
+
+//! [1]
+        QRectF r1(100, 200, 11, 16);
+        QRectF r2(QPoint(100, 200), QSize(11, 16));
+//! [1]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_corelib_tools_qrect {
+	public static void main(String args[])
+	{
+		QApplication.initialize(args);
+		{
+//! [0]
+		QRect r1 = new QRect(100, 200, 11, 16);
+		QRect r2 = new QRect(new QPoint(100, 200), new QSize(11, 16));
+//! [0]
+		}
+		{
+//! [1]
+		QRectF r1 = new QRectF(100, 200, 11, 16);
+		QRectF r2 = new QRectF(new QPointF(100, 200), new QSizeF(11, 16));
+//! [1]
+		}
+
+	}
+}
