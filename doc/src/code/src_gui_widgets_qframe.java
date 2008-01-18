f07062ab14ca5db8359b0d0d4b1055d30af23434
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:45:00 +0100

    Argh. Now I actually changed it.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292544]

--- doc/src/code/src_gui_widgets_qframe.java
+++ doc/src/code/src_gui_widgets_qframe.java
@@ -21,15 +21,18 @@ import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
 
 
-public class src_gui_widgets_qframe {
+public class src_gui_widgets_qframe extends QWidget {
     public static void main(String args[]) {
         QApplication.initialize(args);
+    }
+
+public void foo() {
 //! [0]
-    QLabel label = new QLabel();
+    QLabel label = new QLabel(this);
     label.setFrameStyle(QFrame.Shape.Panel.value() | QFrame.Shadow.Raised.value());
     label.setLineWidth(2);
 
-    QProgressBar pbar = new QProgressBar();
+    QProgressBar pbar = new QProgressBar(this);
     label.setFrameStyle(QFrame.Shape.NoFrame.value());
 //! [0]
 
