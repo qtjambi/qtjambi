a20827919710abb78392484364616730f261b8f7
qt-user <qt-info@nokia.com>
2008-01-18 10:50:37 +0100

    It compiles, had to remove this though.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292419]

---
+++ doc/src/code/src_gui_widgets_qpushbutton.java
@@ -0,0 +1,21 @@
+/*   Ported from: src.gui.widgets.qpushbutton.cpp
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qpushbutton {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+//! [0]
+        QPushButton button = new QPushButton("Download");
+//! [0]
+
+
+    }
+}
