3b3121aaaee963daca9042d2a0b9a9ef9011df1f
Trond Kjernaasen <trond@trolltech.com>
2008-01-18 16:03:22 +0100

    No QString in Java - so no porting.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292802]

--- doc/src/code/src_corelib_tools_qstring.java
+++ doc/src/code/src_corelib_tools_qstring.java
@@ -1,56 +1,3 @@
-/*   Ported from: src.corelib.tools.qstring.cpp
-<snip>
-//! [0]
-        DEFINES += QT_NO_CAST_FROM_ASCII \
-                   QT_NO_CAST_TO_ASCII
-//! [0]
-
-
-//! [1]
-        QString url = QLatin1String("http://www.unicode.org/");
-//! [1]
-
-
-//! [2]
-        double d = 12.34;
-        QString str = QString("delta: %1").arg(d, 0, 'E', 3);
-        // str == "delta: 1.234E+01"
-//! [2]
-
-
-//! [3]
-        if (str == "auto" || str == "extern"
-                || str == "static" || str == "register") {
-            ...
-        }
-//! [3]
-
-
-//! [4]
-        if (str == QString("auto") || str == QString("extern")
-                || str == QString("static") || str == QString("register")) {
-            ...
-        }
-//! [4]
-
-
-//! [5]
-        if (str == QLatin1String("auto")
-                || str == QLatin1String("extern")
-                || str == QLatin1String("static")
-                || str == QLatin1String("register") {
-            ...
-        }
-//! [5]
-
-
-//! [6]
-        QLabel *label = new QLabel(QLatin1String("MOD"), this);
-//! [6]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -63,6 +10,8 @@ import com.trolltech.qt.svg.*;
 public class src_corelib_tools_qstring {
     public static void main(String args[]) {
         QApplication.initialize(args);
+
+/* Not relevant in Java - no QString class
 //! [0]
         DEFINES += QT_NO_CAST_FROM_ASCII \
                    QT_NO_CAST_TO_ASCII
@@ -108,9 +57,9 @@ public class src_corelib_tools_qstring {
 
 
 //! [6]
-        QLabel abel = new QLabel(QLatin1String("MOD"), this);
+        QLabel label = new QLabel(QLatin1String("MOD"), this);
 //! [6]
-
+*/
 
     }
 }
