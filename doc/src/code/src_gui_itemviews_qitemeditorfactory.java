78511447abfd13df92d127c4638761308ab60aaa
gunnar <gunnar@THAUMASIA>
2008-01-18 17:15:06 +0100

    hm...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292851]

---
+++ doc/src/code/src_gui_itemviews_qitemeditorfactory.java
@@ -0,0 +1,84 @@
+/*   Ported from: src.gui.itemviews.qitemeditorfactory.cpp
+<snip>
+//! [0]
+        Q_PROPERTY(QColor color READ color WRITE setColor USER true)
+//! [0]
+
+
+//! [1]
+    QItemEditorCreator<MyEditor> *itemCreator =
+        new QItemEditorCreator<MyEditor>("myProperty");
+
+    QItemEditorFactory *factory = new QItemEditorFactory;
+//! [1]
+
+
+//! [2]
+    QItemEditorFactory *editorFactory = new QItemEditorFactory;
+    QItemEditorCreatorBase *creator = new QStandardItemEditorCreator<MyFancyDateTimeEdit>();
+    editorFactory->registerEditor(QVariant::DateType, creator);
+//! [2]
+
+
+//! [3]
+	Q_PROPERTY(QColor color READ color WRITE setColor USER true)	
+//! [3]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_itemviews_qitemeditorfactory {
+
+    QColor color;
+
+//! [0]
+    @QtPropertyReader
+    @QtPropertyUser
+    public QColor color() { return color; }
+
+    @QtPropertyWriter
+    public void setColor(QColor color) { this.color = color; }
+//! [0]
+
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+
+
+        /*
+//! [1]
+    QItemEditorCreator itemCreator =
+        new QItemEditorCreator<MyEditor>("color");
+
+    QItemEditorFactory factory = new QItemEditorFactory;
+//! [1]
+
+
+//! [2]
+    QItemEditorFactory editorFactory = new QItemEditorFactory;
+    QItemEditorCreatorBase creator = new QStandardItemEditorCreator<MyFancyDateTimeEdit>();
+    editorFactory.registerEditor(QVariant.DateType, creator);
+//! [2]
+
+
+//! [3]
+    @QtPropertyReader
+    @QtPropertyUser
+    public QColor color() { return color; }
+
+    @QtPropertyWriter
+    public void setColor(QColor color) { this.color = color); }
+//! [3]
+
+*/
+
+    }
+}
