b371cdae2dac96bcc383b5dd5d7be09bf67fedd1
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:02:58 +0100

    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292505]

---
+++ doc/src/code/src_gui_widgets_qabstractspinbox.java
@@ -0,0 +1,41 @@
+/*   Ported from: src.gui.widgets.qabstractspinbox.cpp
+<snip>
+//! [0]
+        QSpinBox *spinBox = new QSpinBox(this);
+        spinBox->setRange(0, 100);
+        spinBox->setWrapping(true);
+        spinBox->setValue(100);
+        spinBox->stepBy(1);
+        // value is 0
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qabstractspinbox extends QWidget {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+    }
+  
+    public void foo() {
+//! [0]
+        QSpinBox spinBox = new QSpinBox(this);
+        spinBox.setRange(0, 100);
+        spinBox.setWrapping(true);
+        spinBox.setValue(100);
+        spinBox.stepBy(1);
+        // value is 0
+//! [0]
+
+
+    }
+}
