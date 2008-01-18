4c66f72e676cd4f57788b398635eccdb4f836b3c
Jens Bache-Wiig <jbache@trolltech.com>
2008-01-18 12:45:22 +0100

    submit java code
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292545]

--- doc/src/code/src_gui_text_qfont.java
+++ doc/src/code/src_gui_text_qfont.java
@@ -44,33 +44,28 @@ public class src_gui_text_qfont {
     public static void main(String args[]) {
         QApplication.initialize(args);
 //! [0]
-        QFont serifFont("Times", 10, QFont.Bold);
-        QFont sansFont("Helvetica [Cronyx]", 12);
+        QFont serifFont = new QFont("Times", 10, QFont.Weight.Bold.value());
+        QFont sansFont = new QFont("Helv etica [Cronyx]", 12);
 //! [0]
-
-
+        {
 //! [1]
-        QFont f("Helvetica");
+        QFont font = new QFont("Helvetica");
 //! [1]
-
-
+        }
 //! [2]
-        QFont f("Helvetica [Cronyx]");
+        QFont font = new QFont("Helvetica [Cronyx]");
 //! [2]
-
 
 //! [3]
-        QFontInfo info(f1);
-        Stringsfamily = info.family();
+        QFontInfo info = new QFontInfo(font);
+        String family = info.family();
 //! [3]
 
 
 //! [4]
-        QFontMetrics fm(f1);
+        QFontMetrics fm = new QFontMetrics(font);
         int textWidthInPixels = fm.width("How many pixels wide is this text?");
         int textHeightInPixels = fm.height();
 //! [4]
-
-
     }
 }
