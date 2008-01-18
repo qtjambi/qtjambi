80ccf1bb3cee1320fb7143ced71d1c2d1724071c
Kavindra Devi Palaraja <kdpalara@trolltech.com>
2008-01-18 11:33:42 +0100

    Fixes:    Snippet successfully ported from C++ to Java
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292461]

--- doc/src/code/src_gui_widgets_qtextedit.java
+++ doc/src/code/src_gui_widgets_qtextedit.java
@@ -1,28 +1,4 @@
 /*   Ported from: src.gui.widgets.qtextedit.cpp
-<snip>
-//! [0]
-  void MyTextEdit::contextMenuEvent(QContextMenuEvent *event)
-  {
-      QMenu *menu = createStandardContextMenu();
-      menu->addAction(tr("My Menu Item"));
-      //...
-      menu->exec(event->globalPos());
-      delete menu;
-  }
-//! [0]
-
-
-//! [1]
-    edit->textCursor().insertText(text);
-//! [1]
-
-
-//! [2]
-    edit->textCursor().insertHtml(fragment);
-//! [2]
-
-
-</snip>
 */
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
@@ -33,7 +9,7 @@ import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
 
 
-public class src_gui_widgets_qtextedit {
+public class src_gui_widgets_qtextedit extends QWidget {
     static void main(String args[]) {
         QApplication.initialize(args);
 
@@ -54,7 +30,7 @@ public class src_gui_widgets_qtextedit {
     }
 
 //! [0]
-    void contextMenuEvent(QContextMenuEvent event) {
+    protected void contextMenuEvent(QContextMenuEvent event) {
         QMenu menu = createStandardContextMenu();
         menu.addAction(tr("My Menu Item"));
         //...
