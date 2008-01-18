a33f9055a6a804c583949345edc6259a624c0ad4
qt-user <qt-info@nokia.com>
2008-01-18 14:35:08 +0100

    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292696]

---
+++ doc/src/code/src_gui_util_qdesktopservices.java
@@ -0,0 +1,33 @@
+
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_util_qdesktopservices {
+
+
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+
+//! [0]
+		QDesktopServices.setUrlHandler("help", new QDesktopServices.UrlHandler() {
+			public void handle() {
+				// respond to url...
+			}
+			});
+//! [0]
+
+
+	/*
+//! [1]
+    mailto:user@foo.com?subject=Testody=Just a test
+//! [1]
+	*/
+
+    }
+}
