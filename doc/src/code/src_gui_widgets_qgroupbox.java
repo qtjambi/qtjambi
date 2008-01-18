8674fd39a7958a779ba3c1af5f646b8f37a93eae
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:31:32 +0100

    Compiles now
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292532]

---
+++ doc/src/code/src_gui_widgets_qgroupbox.java
@@ -0,0 +1,31 @@
+/*   Ported from: src.gui.widgets.qgroupbox.cpp
+<snip>
+//! [0]
+        g->setTitle("&User information");
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qgroupbox {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+    }
+  
+public void foo(QGroupBox g) {
+//! [0]
+        g.setTitle("&User information");
+//! [0]
+
+
+    }
+}
