052bbeb532d4b6e899a803430308ba6a737c2e2b
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:28:45 +0100

    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292530]

---
+++ doc/src/code/src_gui_widgets_qframe.java
@@ -0,0 +1,38 @@
+/*   Ported from: src.gui.widgets.qframe.cpp
+<snip>
+//! [0]
+    QLabel label(...);
+    label.setFrameStyle(QFrame::Panel | QFrame::Raised);
+    label.setLineWidth(2);
+
+    QProgressBar pbar(...);
+    label.setFrameStyle(QFrame::NoFrame);
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qframe {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+//! [0]
+    QLabel label = new QLabel();
+    label.setFrameStyle(QFrame.Shape.Panel.value() | QFrame.Shadow.Raised.value());
+    label.setLineWidth(2);
+
+    QProgressBar pbar = new QProgressBar();
+    label.setFrameStyle(QFrame.Shape.NoFrame.value());
+//! [0]
+
+
+    }
+}
