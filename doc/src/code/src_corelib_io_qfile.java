8741007002ed74c9aa18b743e5eef099caa7b3f8
qt-user <qt-info@nokia.com>
2008-01-18 11:40:28 +0100

    Port to java
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292464]

--- doc/src/code/src_corelib_io_qfile.java
+++ doc/src/code/src_corelib_io_qfile.java
@@ -1,44 +1,3 @@
-/*   Ported from: src.corelib.io.qfile.cpp
-<snip>
-//! [0]
-        QFile file;
-        QDir::setCurrent("/tmp");
-        file.setFileName("readme.txt");
-        QDir::setCurrent("/home");
-        file.open(QIODevice::ReadOnly);      // opens "/home/readme.txt" under Unix
-//! [0]
-
-
-//! [1]
-        QByteArray myEncoderFunc(const QString &fileName);
-//! [1]
-
-
-//! [2]
-        QString myDecoderFunc(const QByteArray &localFileName);
-//! [2]
-
-
-//! [3]
-        #include <stdio.h>
-
-        void printError(const char* msg)
-        {
-            QFile file;
-            file.open(stderr, QIODevice::WriteOnly);
-            file.write(msg, qstrlen(msg));        // write to stderr
-            file.close();
-        }
-//! [3]
-
-
-//! [4]
-    CONFIG += console
-//! [4]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -52,41 +11,12 @@ public class src_corelib_io_qfile {
     public static void main(String args[]) {
         QApplication.initialize(args);
 //! [0]
-        QFile file;
+        QFile file = new QFile();
         QDir.setCurrent("/tmp");
         file.setFileName("readme.txt");
         QDir.setCurrent("/home");
-        file.open(QIODevice.ReadOnly);      // opens "/home/readme.txt" under Unix
+        file.open(QIODevice.OpenModeFlag.ReadOnly);      // opens "/home/readme.txt" under Unix
 //! [0]
-
-
-//! [1]
-        QByteArray myEncoderFunc(StringsileName);
-//! [1]
-
-
-//! [2]
-        StringsmyDecoderFunc(QByteArray ocalFileName);
-//! [2]
-
-
-//! [3]
-        #include <stdio.h>
-
-        void printError(char* msg)
-        {
-            QFile file;
-            file.open(stderr, QIODevice.WriteOnly);
-            file.write(msg, qstrlen(msg));        // write to stderr
-            file.close();
-        }
-//! [3]
-
-
-//! [4]
-    CONFIG += console
-//! [4]
-
 
     }
 }
