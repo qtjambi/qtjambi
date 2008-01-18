77ae99ef3dfb312f7ca4a9a30b48569ee917f704
gunnar <gunnar@THAUMASIA>
2008-01-18 10:40:48 +0100

    compile
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292410]

--- doc/src/code/src_gui_painting_qbrush.java
+++ doc/src/code/src_gui_painting_qbrush.java
@@ -1,44 +1,28 @@
-/*   Ported from: src.gui.painting.qbrush.cpp
-<snip>
-//! [0]
-        QPainter painter(this);
+import com.trolltech.qt.gui.*;
 
-        painter.setBrush(Qt::cyan);
-        painter.setPen(Qt::darkCyan);
+public class src_gui_painting_qbrush extends QWidget {
+
+    protected void paintEvent(QPaintEvent e) {
+//! [0]
+        QPainter painter = new QPainter(this);
+
+        painter.setBrush(QColor.cyan);
+        painter.setPen(QColor.darkCyan);
         painter.drawRect(0, 0, 100,100);
 
-        painter.setBrush(Qt::NoBrush);
-        painter.setPen(Qt::darkGreen);
+        painter.setBrush(QBrush.NoBrush);
+        painter.setPen(QColor.darkGreen);
         painter.drawRect(40, 40, 100, 100);
 //! [0]
 
+    }
 
-</snip>
-*/
-import com.trolltech.qt.*;
-import com.trolltech.qt.core.*;
-import com.trolltech.qt.gui.*;
-import com.trolltech.qt.xml.*;
-import com.trolltech.qt.network.*;
-import com.trolltech.qt.sql.*;
-import com.trolltech.qt.svg.*;
-
-
-public class src_gui_painting_qbrush {
     public static void main(String args[]) {
         QApplication.initialize(args);
-//! [0]
-        QPainter painter(this);
 
-        painter.setBrush(Qt.cyan);
-        painter.setPen(Qt.darkCyan);
-        painter.drawRect(0, 0, 100,100);
+        QWidget w = new src_gui_painting_qbrush();
+        w.show();
 
-        painter.setBrush(Qt.NoBrush);
-        painter.setPen(Qt.darkGreen);
-        painter.drawRect(40, 40, 100, 100);
-//! [0]
-
-
+        QApplication.exec();
     }
 }
