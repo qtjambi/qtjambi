b760ed85047655bccad9755bade6f0def20b6235
qt <qt@manta>
2008-01-18 11:28:22 +0100

    (msorvig) port to java.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292453]

--- doc/src/code/src_corelib_thread_qmutex.java
+++ doc/src/code/src_corelib_thread_qmutex.java
@@ -1,162 +1,3 @@
-/*   Ported from: src.corelib.thread.qmutex.cpp
-<snip>
-//! [0]
-        int number = 6;
-
-        void method1()
-        {
-            number *= 5;
-            number /= 4;
-        }
-
-        void method2()
-        {
-            number *= 3;
-            number /= 2;
-        }
-//! [0]
-
-
-//! [1]
-        // method1()
-        number *= 5;        // number is now 30
-        number /= 4;        // number is now 7
-
-        // method2()
-        number *= 3;        // number is now 21
-        number /= 2;        // number is now 10
-//! [1]
-
-
-//! [2]
-        // Thread 1 calls method1()
-        number *= 5;        // number is now 30
-
-        // Thread 2 calls method2().
-        //
-        // Most likely Thread 1 has been put to sleep by the operating
-        // system to allow Thread 2 to run.
-        number *= 3;        // number is now 90
-        number /= 2;        // number is now 45
-
-        // Thread 1 finishes executing.
-        number /= 4;        // number is now 11, instead of 10
-//! [2]
-
-
-//! [3]
-        QMutex mutex;
-        int number = 6;
-
-        void method1()
-        {
-            mutex.lock();
-            number *= 5;
-            number /= 4;
-            mutex.unlock();
-        }
-
-        void method2()
-        {
-            mutex.lock();
-            number *= 3;
-            number /= 2;
-            mutex.unlock();
-        }
-//! [3]
-
-
-//! [4]
-        int complexFunction(int flag)
-        {
-            mutex.lock();
-
-            int retVal = 0;
-
-            switch (flag) {
-            case 0:
-            case 1:
-                mutex.unlock();
-                return moreComplexFunction(flag);
-            case 2:
-                {
-                    int status = anotherFunction();
-                    if (status < 0) {
-                        mutex.unlock();
-                        return -2;
-                    }
-                    retVal = status + flag;
-                }
-                break;
-            default:
-                if (flag > 10) {
-                    mutex.unlock();
-                    return -1;
-                }
-                break;
-            }
-
-            mutex.unlock();
-            return retVal;
-        }
-//! [4]
-
-
-//! [5]
-        int complexFunction(int flag)
-        {
-            QMutexLocker locker(&mutex);
-
-            int retVal = 0;
-
-            switch (flag) {
-            case 0:
-            case 1:
-                return moreComplexFunction(flag);
-            case 2:
-                {
-                    int status = anotherFunction();
-                    if (status < 0)
-                        return -2;
-                    retVal = status + flag;
-                }
-                break;
-            default:
-                if (flag > 10)
-                    return -1;
-                break;
-            }
-
-            return retVal;
-        }
-//! [5]
-
-
-//! [6]
-        class SignalWaiter
-        {
-        private:
-            QMutexLocker locker;
-
-        public:
-            SignalWaiter(QMutex *mutex)
-                : locker(mutex)
-            {
-            }
-
-            void waitForSignal()
-            {
-                ...
-                while (!signalled)
-                    waitCondition.wait(locker.mutex());
-                ...
-            }
-        };
-//! [6]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -169,22 +10,24 @@ import com.trolltech.qt.svg.*;
 public class src_corelib_thread_qmutex {
     public static void main(String args[]) {
         QApplication.initialize(args);
+
+class FooBar1 {
 //! [0]
         int number = 6;
 
-        void method1()
+        public void method1()
         {
             number *= 5;
             number /= 4;
         }
 
-        void method2()
+        public void method2()
         {
             number *= 3;
             number /= 2;
         }
 //! [0]
-
+	public void method3() {
 
 //! [1]
         // method1()
@@ -211,13 +54,14 @@ public class src_corelib_thread_qmutex {
         // Thread 1 finishes executing.
         number /= 4;        // number is now 11, instead of 10
 //! [2]
-
-
+} // method3
+} // class FooBar
+class FooBar2 {
 //! [3]
-        QMutex mutex;
+        QMutex mutex = new QMutex();
         int number = 6;
 
-        void method1()
+        public void method1()
         {
             mutex.lock();
             number *= 5;
@@ -225,7 +69,7 @@ public class src_corelib_thread_qmutex {
             mutex.unlock();
         }
 
-        void method2()
+        public void method2()
         {
             mutex.lock();
             number *= 3;
@@ -233,94 +77,15 @@ public class src_corelib_thread_qmutex {
             mutex.unlock();
         }
 //! [3]
+} // class FooBar2
 
+// QMutexLocker snippets removed:
 
 //! [4]
-        int complexFunction(int flag)
-        {
-            mutex.lock();
-
-            int retVal = 0;
-
-            switch (flag) {
-            case 0:
-            case 1:
-                mutex.unlock();
-                return moreComplexFunction(flag);
-            case 2:
-                {
-                    int status = anotherFunction();
-                    if (status < 0) {
-                        mutex.unlock();
-                        return -2;
-                    }
-                    retVal = status + flag;
-                }
-                break;
-            default:
-                if (flag > 10) {
-                    mutex.unlock();
-                    return -1;
-                }
-                break;
-            }
-
-            mutex.unlock();
-            return retVal;
-        }
 //! [4]
-
-
 //! [5]
-        int complexFunction(int flag)
-        {
-            QMutexLocker locker(utex);
-
-            int retVal = 0;
-
-            switch (flag) {
-            case 0:
-            case 1:
-                return moreComplexFunction(flag);
-            case 2:
-                {
-                    int status = anotherFunction();
-                    if (status < 0)
-                        return -2;
-                    retVal = status + flag;
-                }
-                break;
-            default:
-                if (flag > 10)
-                    return -1;
-                break;
-            }
-
-            return retVal;
-        }
 //! [5]
-
-
 //! [6]
-        class SignalWaiter
-        {
-        private:
-            QMutexLocker locker;
-
-        public:
-            SignalWaiter(QMutex utex)
-                : locker(mutex)
-            {
-            }
-
-            void waitForSignal()
-            {
-                ...
-                while (!signalled)
-                    waitCondition.wait(locker.mutex());
-                ...
-            }
-        };
 //! [6]
 
 
