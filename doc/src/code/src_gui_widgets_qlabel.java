50ed1e860ce8bcdd33baa72d116aea4d44fc957f
qt-user <qt-info@nokia.com>
2008-01-18 13:35:20 +0100

    <ported>
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292604]

--- doc/src/code/src_gui_widgets_qlabel.java
+++ doc/src/code/src_gui_widgets_qlabel.java
@@ -1,33 +1,3 @@
-/*   Ported from: src.gui.widgets.qlabel.cpp
-<snip>
-//! [0]
-    QLabel *label = new QLabel(this);
-    label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-    label->setText("first line\nsecond line");
-    label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
-//! [0]
-
-
-//! [1]
-    QLineEdit* phoneEdit = new QLineEdit(this);
-    QLabel* phoneLabel = new QLabel("&Phone:", this);
-    phoneLabel->setBuddy(phoneEdit);
-//! [1]
-
-
-//! [2]
-    QLineEdit *nameEd  = new QLineEdit(this);
-    QLabel    *nameLb  = new QLabel("&Name:", this);
-    nameLb->setBuddy(nameEd);
-    QLineEdit *phoneEd = new QLineEdit(this);
-    QLabel    *phoneLb = new QLabel("&Phone:", this);
-    phoneLb->setBuddy(phoneEd);
-    // (layout setup not shown)
-//! [2]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -41,26 +11,26 @@ public class src_gui_widgets_qlabel {
     public static void main(String args[]) {
         QApplication.initialize(args);
 //! [0]
-    QLabel abel = new QLabel(this);
-    label.setFrameStyle(QFrame.Panel | QFrame.Sunken);
+    QLabel label = new QLabel();
+    label.setFrameStyle(QFrame.Shadow.Sunken.value() | QFrame.Shape.Panel.value() );
     label.setText("first line\nsecond line");
-    label.setAlignment(Qt.AlignBottom | Qt.AlignRight);
+    label.setAlignment(Qt.AlignmentFlag.AlignBottom, Qt.AlignmentFlag.AlignRight);
 //! [0]
 
 
 //! [1]
-    QLineEdit* phoneEdit = new QLineEdit(this);
-    QLabel* phoneLabel = new QLabel("hone:", this);
+    QLineEdit phoneEdit = new QLineEdit();
+    QLabel phoneLabel = new QLabel("Phone:");
     phoneLabel.setBuddy(phoneEdit);
 //! [1]
 
 
 //! [2]
-    QLineEdit ameEd  = new QLineEdit(this);
-    QLabel    ameLb  = new QLabel("ame:", this);
+    QLineEdit nameEd  = new QLineEdit();
+    QLabel    nameLb  = new QLabel("Name:");
     nameLb.setBuddy(nameEd);
-    QLineEdit honeEd = new QLineEdit(this);
-    QLabel    honeLb = new QLabel("hone:", this);
+    QLineEdit phoneEd = new QLineEdit();
+    QLabel    phoneLb = new QLabel("Phone:");
     phoneLb.setBuddy(phoneEd);
     // (layout setup not shown)
 //! [2]
