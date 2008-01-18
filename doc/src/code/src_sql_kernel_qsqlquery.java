42cf6bd144628a51eb42b50b45b9d67497984a1a
Trond Kjernaasen <trond@trolltech.com>
2008-01-18 15:09:11 +0100

    Ported!
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292731]

--- doc/src/code/src_sql_kernel_qsqlquery.java
+++ doc/src/code/src_sql_kernel_qsqlquery.java
@@ -1,49 +1,5 @@
-/*   Ported from: src.sql.kernel.qsqlquery.cpp
-<snip>
-//! [0]
-        SELECT forename, surname FROM people;
-//! [0]
+import java.util.*;
 
-
-//! [1]
-    QSqlQuery q("select * from employees");
-    QSqlRecord rec = q.record();
-
-    qDebug() << "Number of columns: " << rec.count();
-
-    int nameCol = rec.indexOf("name"); // index of the field "name"
-    while (q.next())
-        qDebug() << q.value(nameCol).toString(); // output all names
-//! [1]
-
-
-//! [2]
-        QSqlQuery q;
-        q.prepare("insert into myTable values (?, ?)");
-
-        QVariantList ints;
-        ints << 1 << 2 << 3 << 4;
-        q.addBindValue(ints);
-
-        QVariantList names;
-        names << "Harald" << "Boris" << "Trond" << QVariant(QVariant::String);
-        q.addBindValue(names);
-
-        if (!q.execBatch())
-            qDebug() << q.lastError();
-//! [2]
-
-
-//! [3]
-        1  Harald
-        2  Boris
-        3  Trond
-        4  NULL
-//! [3]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -56,47 +12,54 @@ import com.trolltech.qt.svg.*;
 public class src_sql_kernel_qsqlquery {
     public static void main(String args[]) {
         QApplication.initialize(args);
+/*
 //! [0]
         SELECT forename, surname FROM people;
 //! [0]
-
-
+*/
+	{
 //! [1]
-    QSqlQuery q("select * from employees");
+    QSqlQuery q = new QSqlQuery("select * from employees");
     QSqlRecord rec = q.record();
 
-    qDebug() << "Number of columns: " << rec.count();
+    System.out.println("Number of columns: " + rec.count());
 
     int nameCol = rec.indexOf("name"); // index of the field "name"
     while (q.next())
-        qDebug() << q.value(nameCol).toString(); // output all names
+        System.out.println(q.value(nameCol).toString()); // output all names
 //! [1]
-
-
+	}
+	{
 //! [2]
-        QSqlQuery q;
+        QSqlQuery q = new QSqlQuery();
         q.prepare("insert into myTable values (?, ?)");
 
-        QVariantList ints;
-        ints << 1 << 2 << 3 << 4;
+	List<Integer> ints = new ArrayList<Integer>();
+        ints.add(1);
+	ints.add(2);
+	ints.add(3);
+	ints.add(4);
         q.addBindValue(ints);
 
-        QVariantList names;
-        names << "Harald" << "Boris" << "Trond" << QVariant(QVariant.String);
+	List<String> names = new ArrayList<String>();
+	names.add("Harald");
+	names.add("Boris");
+	names.add("Trond");
+ 	names.add(null);
         q.addBindValue(names);
 
         if (!q.execBatch())
-            qDebug() << q.lastError();
+            System.out.println(q.lastError());
 //! [2]
-
-
+	}
+/*
 //! [3]
-        1  Harald
-        2  Boris
-        3  Trond
+	1  Harald
+	2  Boris
+	3  Trond
         4  NULL
 //! [3]
-
+*/
 
     }
 }
