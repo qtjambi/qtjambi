81d584f87680078c1885e2de8b8725f8ea0d5ac1
qt-user <qt-info@nokia.com>
2008-01-18 13:44:36 +0100

    ported C++ to Java
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292624]

--- doc/src/code/src_gui_dialogs_qwizard.java
+++ doc/src/code/src_gui_dialogs_qwizard.java
@@ -1,49 +1,3 @@
-/*   Ported from: src.gui.dialogs.qwizard.cpp
-<snip>
-//! [0]
-        int LicenseWizard::nextId() const
-        {
-            switch (currentId()) {
-            case Page_Intro:
-                if (field("intro.evaluate").toBool()) {
-                    return Page_Evaluate;
-                } else {
-                    return Page_Register;
-                }
-            case Page_Evaluate:
-                return Page_Conclusion;
-            case Page_Register:
-                if (field("register.upgradeKey").toString().isEmpty()) {
-                    return Page_Details;
-                } else {
-                    return Page_Conclusion;
-                }
-            case Page_Details:
-                return Page_Conclusion;
-            case Page_Conclusion:
-            default:
-                return -1;
-            }
-        }
-//! [0]
-
-
-//! [1]
-        MyWizard::MyWizard(QWidget *parent)
-            : QWizard(parent)
-        {
-            ...
-            QList<QWizard::WizardButton> layout;
-            layout << QWizard::Stretch << QWizard::BackButton << QWizard::CloseButton
-                   << QWizard::NextButton << QWizard::FinishButton;
-            setButtonLayout(layout);
-            ...
-        }
-//! [1]
-
-
-</snip>
-*/
 import com.trolltech.qt.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
@@ -51,52 +5,59 @@ import com.trolltech.qt.xml.*;
 import com.trolltech.qt.network.*;
 import com.trolltech.qt.sql.*;
 import com.trolltech.qt.svg.*;
+import java.util.List;
+import java.util.LinkedList;
 
+class LicenceWizard extends QWizard {
 
-public class src_gui_dialogs_qwizard {
-    public static void main(String args[]) {
-        QApplication.initialize(args);
+    private static final int Page_Intro = 0;
+    private static final int Page_Evaluate = 1;
+    private static final int Page_Register = 2;
+    private static final int Page_Details = 3;
+    private static final int Page_Conclusion = 4;
+
 //! [0]
-        int LicenseWizard.nextId()
-        {
-            switch (currentId()) {
-            case Page_Intro:
-                if (field("intro.evaluate").toBool()) {
-                    return Page_Evaluate;
-                } else {
-                    return Page_Register;
-                }
-            case Page_Evaluate:
-                return Page_Conclusion;
-            case Page_Register:
-                if (field("register.upgradeKey").toString().isEmpty()) {
-                    return Page_Details;
-                } else {
-                    return Page_Conclusion;
-                }
-            case Page_Details:
-                return Page_Conclusion;
-            case Page_Conclusion:
-            default:
-                return -1;
+    public int nextId()
+    {
+        switch (currentId()) {
+        case Page_Intro:
+            if (QVariant.toBoolean(field("intro.evaluate"))) {
+                return Page_Evaluate;
+            } else {
+                return Page_Register;
             }
+        case Page_Evaluate:
+            return Page_Conclusion;
+        case Page_Register:
+            if (QVariant.toString(field("register.upgradeKey")).length() == 0) {
+                return Page_Details;
+            } else {
+                return Page_Conclusion;
+            }
+        case Page_Details:
+            return Page_Conclusion;
+        case Page_Conclusion:
+        default:
+            return -1;
         }
-//! [0]
-
-
-//! [1]
-        MyWizard.MyWizard(QWidget arent)
-            : QWizard(parent)
-        {
-            ...
-            QList<QWizard.WizardButton> layout;
-            layout << QWizard.Stretch << QWizard.BackButton << QWizard.CloseButton
-                   << QWizard.NextButton << QWizard.FinishButton;
-            setButtonLayout(layout);
-            ...
-        }
-//! [1]
-
-
     }
+//! [0]
+}
+
+class MyWizard extends QWizard {
+//! [1]
+    public MyWizard(QWidget parent)
+    {
+        super(parent);
+        // ...
+        List<QWizard.WizardButton> layout = new LinkedList<QWizard.WizardButton>();
+        layout.add(QWizard.WizardButton.Stretch);
+        layout.add(QWizard.WizardButton.BackButton);
+        layout.add(QWizard.WizardButton.CancelButton);
+        layout.add(QWizard.WizardButton.NextButton);
+        layout.add(QWizard.WizardButton.FinishButton);
+        setButtonLayout(layout);
+        // ...
+    }
+//! [1]
 }
