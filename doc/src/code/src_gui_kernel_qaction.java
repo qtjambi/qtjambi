e35302e078a468c6502619489685f272bd022a4e
Prasanth Ullattil <prasanth.ullattil@trolltech.com>
2008-01-18 13:47:46 +0100

    compiled
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292629]

---
+++ doc/src/code/src_gui_kernel_qaction.java
@@ -0,0 +1,40 @@
+/*   Ported from: src.gui.kernel.qaction.cpp
+<snip>
+//! [0]
+    QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_DontShowIconsInMenus);  // Icons are *no longer shown* in menus
+    // ...
+    QAction *myAction = new QAction();
+    // ...
+    myAction->setIcon(SomeIcon);
+    myAction->setIconVisibleInMenu(true);   // Icon *will* be shown in menus for *this* action.
+//! [0]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_kernel_qaction {
+    public static void main(String args[]) {
+    QApplication.initialize(args);
+    QWidget window = new QWidget();
+    QIcon SomeIcon = new QIcon("myicon.xpm");
+//! [0]
+    QApplication.initialize(args);
+    QApplication.setAttribute(Qt.ApplicationAttribute.AA_DontShowIconsInMenus);  // Icons are *no longer shown* in menus
+    // ...
+    QAction myAction = new QAction(window);
+    // ...
+    myAction.setIcon(SomeIcon);
+    myAction.setIconVisibleInMenu(true);   // Icon *will* be shown in menus for *this* action.
+//! [0]
+    }
+}
