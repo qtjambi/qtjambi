c842ee5c59871c7a70e0c65f9c6cb32c3e223932
Kai Köhne <kkoehne@trolltech.com>
2008-01-18 12:35:22 +0100

    Compiles now
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292539]

---
+++ doc/src/code/src_gui_widgets_qmenubar.java
@@ -0,0 +1,41 @@
+/*   Ported from: src.gui.widgets.qmenubar.cpp
+<snip>
+//! [0]
+      menubar->addMenu(fileMenu);
+//! [0]
+
+
+//! [1]
+        QMenuBar *menuBar = new QMenuBar(0);
+//! [1]
+
+
+</snip>
+*/
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+import com.trolltech.qt.xml.*;
+import com.trolltech.qt.network.*;
+import com.trolltech.qt.sql.*;
+import com.trolltech.qt.svg.*;
+
+
+public class src_gui_widgets_qmenubar {
+    public static void main(String args[]) {
+        QApplication.initialize(args);
+    }
+public void foo(QMenuBar menubar, QMenu fileMenu) {
+//! [0]
+      menubar.addMenu(fileMenu);
+//! [0]
+}
+
+public void foo() {
+//! [1]
+        QMenuBar menuBar = new QMenuBar();
+//! [1]
+
+
+    }
+}
