1198abafb03e5b7b56bae612231e88c3667fefd3
qt-user <qt-info@nokia.com>
2008-01-18 15:15:22 +0100

    Ported QtJambi snippets.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292739]

--- doc/src/code/src_network_kernel_qhostaddress.java
+++ doc/src/code/src_network_kernel_qhostaddress.java
@@ -25,12 +25,13 @@ import com.trolltech.qt.svg.*;
 public class src_network_kernel_qhostaddress {
     public static void main(String args[]) {
         QApplication.initialize(args);
+	QHostAddress hostAddr = new QHostAddress();
 //! [0]
-        Q_IPV6ADDR addr = hostAddr.toIPv6Address();
+        QIPv6Address addr = hostAddr.toIPv6Address();
         // addr contains 16 unsigned characters
 
         for (int i = 0; i < 16; ++i) {
-            // process addr[i]
+            // process addr.c[i]
         }
 //! [0]
 
--- doc/src/code/src_network_kernel_qhostinfo.java
+++ doc/src/code/src_network_kernel_qhostinfo.java
@@ -67,57 +67,72 @@ import com.trolltech.qt.svg.*;
 public class src_network_kernel_qhostinfo {
     public static void main(String args[]) {
         QApplication.initialize(args);
-//! [0]
-        // To find the IP address of www.trolltech.com
-        QHostInfo.lookupHost("www.trolltech.com",
-                              this, SLOT(printResults(QHostInfo)));
+    }
+    public class MyWidget {
 
-        // To find the host name for 4.2.2.1
-        QHostInfo.lookupHost("4.2.2.1",
-                              this, SLOT(printResults(QHostInfo)));
+	public void foo1()
+	{
+//! [0]
+	    // To find the IP address of www.trolltech.com
+	    QHostInfo.lookupHost("www.trolltech.com",
+				 this, "printResults(QHostInfo)");
+	    
+	    // To find the host name for 4.2.2.1
+	    QHostInfo.lookupHost("4.2.2.1",
+				 this, "printResults(QHostInfo)");
 //! [0]
 
 
 //! [1]
-        QHostInfo info = QHostInfo.fromName("www.trolltech.com");
+	    QHostInfo info = QHostInfo.fromName("www.trolltech.com");
 //! [1]
 
 
 //! [2]
-        QHostInfo.lookupHost("www.kde.org",
-                              this, SLOT(lookedUp(QHostInfo)));
+	    QHostInfo.lookupHost("www.kde.org",
+				 this, "lookedUp(QHostInfo)");
 //! [2]
 
 
+//! [4]
+	    QHostInfo.lookupHost("4.2.2.1",
+				 this, "lookedUp(QHostInfo)");
+//! [4]
+
+	}
+	public void foo2()
+	{
+
+//! [5]
+	    QHostInfo info = new QHostInfo();
+	    // ...
+	    if (!info.addresses().isEmpty()) {
+		QHostAddress address = info.addresses().get(0);
+		// use the first IP address
+	    }
+//! [5]
+
+	}
+
+
+	private QHostInfo host;
 //! [3]
-        void MyWidget.lookedUp(QHostInfo ost)
+        public void lookedUp(QHostInfo ost)
         {
-            if (host.error() != QHostInfo.NoError) {
-                qDebug() << "Lookup failed:" << host.errorString();
+            if (host.error() != QHostInfo.HostInfoError.NoError) {
+                System.out.println("Lookup failed:");
+		System.out.println(host.errorString());
                 return;
             }
 
-            for (QHostAddress address, host.addresses())
-                qDebug() << "Found address:" << address.toString();
+            for (int c = 0; c < host.addresses().size(); c++) {
+		QHostAddress address = host.addresses().get(c);
+                System.out.println("Found address:");
+		System.out.println(address.toString());
+	    }
         }
 //! [3]
 
-
-//! [4]
-        QHostInfo.lookupHost("4.2.2.1",
-                              this, SLOT(lookedUp(QHostInfo)));
-//! [4]
-
-
-//! [5]
-        QHostInfo info;
-        ...
-        if (!info.addresses().isEmpty()) {
-            QHostAddress address = info.addresses().first();
-            // use the first IP address
-        }
-//! [5]
-
-
     }
+
 }
