e81a99e6cc74fec08355575b890d995bde6818d5
hfroilan <a@b>
2008-01-21 10:15:12 +0100

    snippet porting
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292917]

--- doc/src/code/src_gui_widgets_qtextbrowser.java
+++ doc/src/code/src_gui_widgets_qtextbrowser.java
@@ -1,25 +1,16 @@
-/*   Ported from: src.gui.widgets.qtextbrowser.cpp
-<snip>
-//! [0]
-    backaction.setToolTip(browser.historyTitle(-1));
-    forwardaction.setToolTip(browser.historyTitle(+1));
-//! [0]
-
-
-</snip>
-*/
-import com.trolltech.qt.*;
-import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
-import com.trolltech.qt.xml.*;
-import com.trolltech.qt.network.*;
-import com.trolltech.qt.sql.*;
-import com.trolltech.qt.svg.*;
 
 
 public class src_gui_widgets_qtextbrowser {
     public static void main(String args[]) {
         QApplication.initialize(args);
+        
+        QWidget widget = new QWidget();
+        QAction backaction = new QAction(widget);
+        QAction forwardaction = new QAction(widget);
+        
+        QTextBrowser browser = new QTextBrowser();
+        
 //! [0]
     backaction.setToolTip(browser.historyTitle(-1));
     forwardaction.setToolTip(browser.historyTitle(+1));
