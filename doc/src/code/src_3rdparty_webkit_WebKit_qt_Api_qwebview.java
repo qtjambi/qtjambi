a33f806e8ec4a09745faa8c61b99ff10371a7978
hfroilan <a@b>
2008-01-21 10:16:12 +0100

    Snippet porting
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 292918]

--- doc/src/code/src_3rdparty_webkit_WebKit_qt_Api_qwebview.java
+++ doc/src/code/src_3rdparty_webkit_WebKit_qt_Api_qwebview.java
@@ -1,49 +1,12 @@
-/*   Ported from: src.3rdparty.webkit.WebKit.qt.Api.qwebview.cpp
-<snip>
-//! [0]
-    view->page()->history();
-//! [0]
-
-
-//! [1]
-    view->page()->settings();
-//! [1]
-
-
-//! [2]
-    view->triggerAction(QWebPage::Copy);
-//! [2]
-
-
-//! [3]
-    view->page()->triggerAction(QWebPage::Stop);
-//! [3]
-
-
-//! [4]
-    view->page()->triggerAction(QWebPage::GoBack);
-//! [4]
-
-
-//! [5]
-    view->page()->triggerAction(QWebPage::GoForward);
-//! [5]
-
-
-</snip>
-*/
-import com.trolltech.qt.*;
-import com.trolltech.qt.core.*;
-import com.trolltech.qt.gui.*;
-import com.trolltech.qt.xml.*;
-import com.trolltech.qt.network.*;
-import com.trolltech.qt.sql.*;
-import com.trolltech.qt.svg.*;
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.webkit.QWebPage;
+import com.trolltech.qt.webkit.QWebView;
 
 
 public class src_3rdparty_webkit_WebKit_qt_Api_qwebview {
     public static void main(String args[]) {
         QApplication.initialize(args);
+        QWebView view = new QWebView();
 //! [0]
     view.page().history();
 //! [0]
@@ -55,22 +18,22 @@ public class src_3rdparty_webkit_WebKit_qt_Api_qwebview {
 
 
 //! [2]
-    view.triggerAction(QWebPage.Copy);
+    view.triggerAction(QWebPage.WebAction.Copy);
 //! [2]
 
 
 //! [3]
-    view.page().triggerAction(QWebPage.Stop);
+    view.page().triggerAction(QWebPage.WebAction.Stop);
 //! [3]
 
 
 //! [4]
-    view.page().triggerAction(QWebPage.GoBack);
+    view.page().triggerAction(QWebPage.WebAction.GoBack);
 //! [4]
 
 
 //! [5]
-    view.page().triggerAction(QWebPage.GoForward);
+    view.page().triggerAction(QWebPage.WebAction.GoForward);
 //! [5]
 
 
