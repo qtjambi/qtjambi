9377c4bbbc5cc13ed7d3877489c2eb7d73ad9a85
eblomfel <eblomfel@SpareRibs>
2008-01-22 11:03:00 +0100

    Oops!
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 293140]

---
+++ qtjambi_core/qtjambiconcurrent_p.h
@@ -0,0 +1,25 @@
+#ifndef QTJAMBICONCURRENT_P_H
+#define QTJAMBICONCURRENT_P_H
+
+#ifndef QT_NO_CONCURRENT
+
+#include <qtjambi_core.h>
+#include <QList>
+#include <QFutureWatcher>
+
+class FutureSequenceCleanUp: public QFutureWatcher<void> {
+    Q_OBJECT
+public:
+    FutureSequenceCleanUp(QList<JObjectWrapper> *sequence);
+    ~FutureSequenceCleanUp();
+
+private slots:
+    void cleanUp();
+
+private:
+    QList<JObjectWrapper> *m_sequence;
+};
+
+#endif QT_NO_CONCURRENT
+
+#endif
