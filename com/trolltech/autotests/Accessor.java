8140b50564e06e039e6e73cdc9a7464f7453f8f7
eblomfel <eblomfel@SpareRibs>
2008-02-05 13:47:38 +0100

    Move out Accessor to compile with newer Eclipse Compiler.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 295172]

---
+++ com/trolltech/autotests/Accessor.java
@@ -0,0 +1,20 @@
+package com.trolltech.autotests;
+
+import com.trolltech.qt.QSignalEmitter.AbstractSignal;
+import com.trolltech.qt.QSignalEmitter.Signal0;
+import com.trolltech.qt.QSignalEmitter.Signal1;
+import com.trolltech.qt.core.QObject;
+
+class Accessor extends QObject
+{    
+    @SuppressWarnings("unchecked") 
+    public static void emit_signal(AbstractSignal signal, Object ... args)
+    {        
+        if (signal instanceof Signal0)
+            ((Signal0) signal).emit();
+        else if (signal instanceof Signal1)
+            ((Signal1) signal).emit(args[0]);
+        else
+            throw new RuntimeException("Implement more classes");
+    }
+}
--- com/trolltech/autotests/TestClassFunctionality.java
+++ com/trolltech/autotests/TestClassFunctionality.java
@@ -114,20 +114,6 @@ class EventReceiver extends QWidget {
     }
 }
 
-class Accessor extends QObject
-{    
-    @SuppressWarnings("unchecked") 
-    public static void emit_signal(AbstractSignal signal, Object ... args)
-    {        
-        if (signal instanceof Signal0)
-            ((Signal0) signal).emit();
-        else if (signal instanceof Signal1)
-            ((Signal1) signal).emit(args[0]);
-        else
-            throw new RuntimeException("Implement more classes");
-    }
-}
-
 public class TestClassFunctionality extends QApplicationTest {
     
     public static void main(String args[]) {
