3401a77c03bfda83643c153257d0c5a83a80bd70
gunnar <gunnar@THAUMASIA>
2008-02-25 17:25:39 +0100

    some batch magic to set up designer.bat
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 298567]

--- dist/win/designer.bat
+++ dist/win/designer.bat
@@ -6,7 +6,13 @@ set OLD_CLASSPATH=%CLASSPATH%
 
 set QT_PLUGIN_PATH=%cd%\plugins
 set PATH=%cd%\bin
-set CLASSPATH=%cd%;%cd%\qtjambi.jar
+
+dir /b qtjambi-4*.jar > .tmp
+set /p QTJAMBI_JAR=< .tmp
+del .tmp
+set VERSION=%QTJAMBI_JAR:~8,8%
+
+set CLASSPATH=%cd%\qtjambi-%VERSION%.jar;%cd%\qtjambi-designer-%VERSION%.jar;%cd%\qtjambi-examples-%VERSION%.jar
 
 "%cd%\bin\designer"
 
@@ -17,3 +23,4 @@ set CLASSPATH=%OLD_CLASSPATH%
 set OLD_PATH=
 set OLD_PLUGIN_PATH=
 set OLD_CLASSPATH=
+set QTJAMBI_JAR=
