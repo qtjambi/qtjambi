94c5b141af990412ad68547cf1958bbf5c04c35b
gunnar <gunnar@THAUMASIA>
2008-03-04 15:50:14 +0100

    figure out current directory
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 299713]

--- set_qtjambi_env.bat
+++ set_qtjambi_env.bat
@@ -1,21 +1,22 @@
 @echo off
 
-dir /b qtjambi-4*.jar > .tmp
+dir /b %~dp0qtjambi-4*.jar > .tmp
 if not "%errorlevel%" == "0" goto error
 set /p QTJAMBI_JAR=< .tmp
 set QTJAMBI_VERSION=%QTJAMBI_JAR:~8,8%
 
-dir /b qtjambi-win*.jar > .tmp
+dir /b %~dp0qtjambi-win*.jar > .tmp
 set /p QTJAMBI_PLATFORM_JAR=< .tmp
 
-set CLASSPATH=%cd%\%QTJAMBI_JAR%.jar;%cd%\%QTJAMBI_PLATFORM_JAR%;qtjambi-examples-%QTJAMBI_VERSION%.jar;%classpath%
+set CLASSPATH=%QTJAMBI_JAR%.jar;%QTJAMBI_PLATFORM_JAR%;%~dp0qtjambi-examples-%QTJAMBI_VERSION%.jar;%classpath%
 
 echo Setting up environment for Qt Jambi %QTJAMBI_VERSION%...
 goto cleanup
 
 :error
-echo No Qt Jambi .jars found...
-echo This script needs to be run from the Qt Jambi directory
+echo Failed to setup environment for building Qt Jambi
+echo No .jars found...
+
 
 :cleanup
 del .tmp
