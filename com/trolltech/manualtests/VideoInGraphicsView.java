ac49b119e8ebcc69a173430917c6b8dee50856b5
gunnar <gunnar@THAUMASIA>
2008-04-01 15:13:18 +0100

    p4i integration Integrate 303807 from 4.4 to main: some minor convenience...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 303808]

--- com/trolltech/manualtests/VideoInGraphicsView.java
+++ com/trolltech/manualtests/VideoInGraphicsView.java
@@ -6,8 +6,8 @@ import com.trolltech.qt.gui.*;
 import com.trolltech.qt.phonon.*;
 
 public class VideoInGraphicsView extends ZoomableGraphicsView {
-	
-	public VideoInGraphicsView() {		
+
+	public VideoInGraphicsView(String file) {
 		setRenderHint(QPainter.RenderHint.Antialiasing);
 
 		// Video widget
@@ -16,35 +16,47 @@ public class VideoInGraphicsView extends ZoomableGraphicsView {
 		// Set up video player
 		MediaObject mediaObject = new MediaObject(this);
 		Phonon.createPath(mediaObject, videoWidget);
-		mediaObject.setCurrentSource(new MediaSource("c:/documents and settings/eblomfel/my documents/my videos/johnrambo_320x144.mpg"));								
+		mediaObject.setCurrentSource(new MediaSource(file));
 		mediaObject.play();
-		
+
 		QGraphicsProxyWidget w = new QGraphicsProxyWidget();
 		w.setWidget(videoWidget);
 		w.setVisible(true);
-		
+
 		// Put ellipse on top
 		w.setZValue(-1);
-		
+
 		// Set up a graphics scene
 		QGraphicsScene scene = new QGraphicsScene();
-		
+
 		scene.addEllipse(10, 10, 100, 100, new QPen(QColor.black), new QBrush(QColor.red)).setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, true);
-		scene.addItem(w);		
+		scene.addItem(w);
 		setScene(scene);
-		
+
 		// Rotate the scene 45 degrees
-		rotate(45);								
-		
+		rotate(45);
+
 	}
-    
+
 	public static void main(String args[]) {
 		QApplication.initialize(args);
-		
-		VideoInGraphicsView view = new VideoInGraphicsView();
+
+                String file = null;
+                if (args.length > 0) {
+                    file = args[0];
+
+                    if (!new QFileInfo(file).exists()) {
+                        System.err.println("File does not exist: " + file);
+                        return;
+                    }
+                } else {
+                    System.err.println("Please specify a movie file...");
+                }
+
+		VideoInGraphicsView view = new VideoInGraphicsView(file);
 		view.show();
-		
+
 		QApplication.exec();
 	}
-	
+
 }
