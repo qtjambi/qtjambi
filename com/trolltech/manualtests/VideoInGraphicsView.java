e488d39dde05a01c3a0b3d608a8ce74346169cc9
eblomfel <eblomfel@SpareRibs>
2008-04-04 12:36:31 +0100

    p4i integration Integrate 304317 from 4.4 to main: updates
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 304318]

--- com/trolltech/manualtests/VideoInGraphicsView.java
+++ com/trolltech/manualtests/VideoInGraphicsView.java
@@ -1,6 +1,6 @@
 package com.trolltech.manualtests;
 
-
+import com.trolltech.qt.opengl.*;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 import com.trolltech.qt.phonon.*;
@@ -14,14 +14,22 @@ public class VideoInGraphicsView extends ZoomableGraphicsView {
         VideoWidget videoWidget = new VideoWidget();
 
         // Set up video player
-        MediaObject mediaObject = new MediaObject(this);
+        final MediaObject mediaObject = new MediaObject(this);
         Phonon.createPath(mediaObject, videoWidget);
         mediaObject.setCurrentSource(new MediaSource(file));
         mediaObject.play();
 
+        mediaObject.finished.connect(new Object() {
+                public void start() {
+                    mediaObject.seek(0);
+                    mediaObject.play();
+                }
+            }, "start()");
+
         QGraphicsProxyWidget w = new QGraphicsProxyWidget();
         w.setWidget(videoWidget);
         w.setVisible(true);
+        setRenderHint(QPainter.RenderHint.SmoothPixmapTransform);
 
         // Put ellipse on top
         w.setZValue(-1);
@@ -31,11 +39,34 @@ public class VideoInGraphicsView extends ZoomableGraphicsView {
 
         scene.addEllipse(10, 10, 100, 100, new QPen(QColor.black), new QBrush(QColor.red)).setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, true);
         scene.addItem(w);
+        QGraphicsTextItem item = new QGraphicsTextItem();
+
+
+        QFile f = new QFile("test.html");
+        f.open(QIODevice.OpenModeFlag.ReadOnly);
+        QByteArray ba = f.readAll();
+        System.err.println(ba.toString());
+        f.close();
+        item.setPos(w.boundingRect().bottomLeft());
+        item.setHtml(ba.toString());
+        item.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, true);
+        item.setVisible(true);
+        item.setTextInteractionFlags(Qt.TextInteractionFlag.TextEditorInteraction);
+        scene.addItem(item);
+
         setScene(scene);
 
         // Rotate the scene 45 degrees
         rotate(45);
 
+        setViewport(new QGLWidget());
+
+        startTimer(100);
+
+    }
+
+    protected void timerEvent(QTimerEvent e) {
+        rotate(0.1);
     }
 
     public static void main(String args[]) {
--- com/trolltech/manualtests/ZoomableGraphicsView.java
+++ com/trolltech/manualtests/ZoomableGraphicsView.java
@@ -1,21 +1,24 @@
 package com.trolltech.manualtests;
 
-import com.trolltech.qt.core.QRectF;
-import com.trolltech.qt.gui.QGraphicsView;
-import com.trolltech.qt.gui.QMatrix;
-import com.trolltech.qt.gui.QWheelEvent;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
 
 public class ZoomableGraphicsView extends QGraphicsView {
     // Zoom on wheel events
     protected void wheelEvent(QWheelEvent event) {
         double scaleFactor = Math.pow(2, -event.delta() / 240.0);
-        QMatrix m = matrix();
-        m.scale(scaleFactor, scaleFactor);
-        double factor = m.mapRect(new QRectF(0, 0, 1, 1)).width();
-        if (factor < 0.07 || factor > 100)
-            return;
 
-        scale(scaleFactor, scaleFactor);
+        if (event.modifiers().isSet(Qt.KeyboardModifier.ControlModifier)) {
+            rotate(event.delta() * 0.01);
+        } else {
+            QMatrix m = matrix();
+            m.scale(scaleFactor, scaleFactor);
+            double factor = m.mapRect(new QRectF(0, 0, 1, 1)).width();
+            if (factor < 0.07 || factor > 100)
+                return;
+
+            scale(scaleFactor, scaleFactor);
+        }
 
     }
 
