f4ca732ab673af421b739167819ed58af4513f87
eblomfel <eblomfel@SpareRibs>
2008-06-04 11:09:02 +0100

    p4i integration Integrate 310220 from 4.4 to main: Integrate 310219 from 4.4.0_01 to 4.4: Update to support new generator usage guidelines
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 310221]

--- autotestlib/build.bat
+++ autotestlib/build.bat
@@ -1,4 +1,4 @@
 cd ..\generator
-release\generator.exe ..\autotestlib\global.h ..\autotestlib\build.txt || debug\generator.exe ..\autotestlib\global.h ..\autotestlib\build.txt
+release\generator.exe --output-directory=.. ..\autotestlib\global.h ..\autotestlib\build.txt || debug\generator.exe --output-directory=.. ..\autotestlib\global.h ..\autotestlib\build.txt
 
 cd ..\autotestlib
