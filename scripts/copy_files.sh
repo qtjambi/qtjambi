0769825c8dc76d177f22ca9a190a3ca0140e3ec4
gunnar <gunnar@THAUMASIA>
2008-06-04 07:44:37 +0100

    p4i integration Integrate 310187 from 4.4 to main: whatever..
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 310188]

---
+++ scripts/copy_files.sh
@@ -0,0 +1,3 @@
+scp qtjambi-*-commercial* gunnar@anarki:public_html/packages/commercial
+scp qtjambi-*-gpl* gunnar@anarki:public_html/packages/gpl
+scp qtjambi-*-eval* gunnar@anarki:public_html/packages/eval
