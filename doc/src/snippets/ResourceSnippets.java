3ca329a7b42fddd794b3dfd20ca3c2e2c9697277
qt-user <qt-info@nokia.com>
2008-06-24 11:40:35 +0100

    p4i integration Integrate 311810 from 4.4 to main: Doc: Added overview on the resource system.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 311811]

---
+++ doc/src/snippets/ResourceSnippets.java
@@ -0,0 +1,45 @@
+
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+
+import java.util.*;
+
+public class ResourceSnippets extends QWidget
+{
+    
+    public ResourceSnippets()
+    {
+    }
+
+    @Override
+    protected void paintEvent(QPaintEvent event)
+    {
+//![0]
+        QFile resourceFile = new QFile("classpath:images/star.png");
+//![0]
+
+//![1]
+        QPixmap pixmap = new QPixmap("classpath:images#/star.png");
+//![1]
+
+//![2]
+        QFile absoluteFilePath = new QFile("classpath:images#/star.png");
+        QFile relativeFilePath = new QFile("classpath:images/star.png");
+//![2]
+
+//![3]
+        QDir dir = new QDir("classpath:images");
+        List<QFileInfo> images = dir.entryInfoList();
+//![3]
+
+    }
+
+    public static void main(String args[])
+    {
+        QApplication.initialize(args);
+
+        new ResourceSnippets().show();
+    }
+}
+
