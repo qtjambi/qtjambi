90a3e31e069f5906c746dfcddee3de62d6f27357
qt-user <qt-info@nokia.com>
2008-06-25 14:08:26 +0100

    p4i integration Integrate 311926 from 4.4 to main: Doc: Ported DnD overview, accompanied by two new ported examples.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 311927]

---
+++ com/trolltech/examples/DraggableIcons.java
@@ -0,0 +1,149 @@
+package com.trolltech.examples;
+
+
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+
+@QtJambiExample(name = "Draggable Icons")
+public class DraggableIcons extends QFrame
+{
+
+//![0]
+    public DraggableIcons()
+    {
+        setMinimumSize(200, 200);
+        setFrameStyle(QFrame.Shadow.Sunken.value() | QFrame.Shape.StyledPanel.value());
+        setAcceptDrops(true);
+
+        QLabel boatIcon = new QLabel(this);
+        boatIcon.setPixmap(new QPixmap("classpath:com/trolltech/examples/images/boat.png"));
+        boatIcon.move(20, 20);
+
+        QLabel carIcon = new QLabel(this);
+        carIcon.setPixmap(new QPixmap("classpath:com/trolltech/examples/images/car.png"));
+        carIcon.move(120, 20);
+
+        QLabel houseIcon = new QLabel(this);
+        houseIcon.setPixmap(new QPixmap("classpath:com/trolltech/examples/images/house.png"));
+        houseIcon.move(20, 120);
+    }
+//![0]
+
+    protected void dragEnterEvent(QDragEnterEvent event)
+    {
+        if (event.mimeData().hasFormat("application/x-dnditemdata")) {
+            if (event.source().equals(this)) {
+                event.setDropAction(Qt.DropAction.MoveAction);
+                event.accept();
+            } else {
+                event.acceptProposedAction();
+            }
+        } else {
+            event.ignore();
+        }
+    }
+
+    protected void dragMoveEvent(QDragMoveEvent event)
+    {
+        if (event.mimeData().hasFormat("application/x-dnditemdata")) {
+            if (event.source().equals(this)) {
+                event.setDropAction(Qt.DropAction.MoveAction);
+                event.accept();
+            } else {
+                event.acceptProposedAction();
+            }
+        } else {
+            event.ignore();
+        }
+    }
+
+    protected void dropEvent(QDropEvent event)
+    {
+        if (event.mimeData().hasFormat("application/x-dnditemdata")) {
+            QByteArray itemData = event.mimeData().data("application/x-dnditemdata");
+            QDataStream dataStream = new QDataStream(itemData, QIODevice.OpenModeFlag.ReadOnly);
+        
+            QPixmap pixmap = new QPixmap();
+            QPoint offset = new QPoint();
+            pixmap.readFrom(dataStream);
+            offset.readFrom(dataStream);
+
+            QLabel newIcon = new QLabel(this);
+            newIcon.setPixmap(pixmap);
+            newIcon.move(event.pos().subtract(offset));
+            newIcon.show();
+            newIcon.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose);
+
+            if (event.source().equals(this)) {
+                event.setDropAction(Qt.DropAction.MoveAction);
+                event.accept();
+            } else {
+                event.acceptProposedAction();
+            }
+        } else {
+            event.ignore();
+        }
+    }
+
+//![1]
+    protected void mousePressEvent(QMouseEvent event)
+    {
+        QLabel child = (QLabel) childAt(event.pos());
+        if (child == null)
+            return;
+
+        QPixmap pixmap = child.pixmap();
+
+        QByteArray itemData = new QByteArray();
+        QDataStream dataStream = new QDataStream(itemData, QIODevice.OpenModeFlag.WriteOnly);
+        pixmap.writeTo(dataStream);
+        event.pos().subtract(child.pos()).writeTo(dataStream);
+
+//! [1]
+
+//! [2]
+        com.trolltech.qt.core.QMimeData mimeData = new com.trolltech.qt.core.QMimeData();
+        mimeData.setData("application/x-dnditemdata", itemData);
+//! [2]
+        
+//! [3]
+        QDrag drag = new QDrag(this);
+        drag.setMimeData(mimeData);
+        drag.setPixmap(pixmap);
+        drag.setHotSpot(event.pos().subtract(child.pos()));
+//! [3]
+
+        QPixmap tempPixmap = new QPixmap(pixmap);
+        QPainter painter = new QPainter();
+        painter.begin(tempPixmap);
+        painter.fillRect(pixmap.rect(), new QBrush(new QColor(127, 127, 127, 127)));
+        painter.end();
+
+        child.setPixmap(tempPixmap);
+
+        if (drag.exec(new Qt.DropActions(Qt.DropAction.CopyAction,
+            Qt.DropAction.MoveAction, Qt.DropAction.CopyAction)) == Qt.DropAction.MoveAction)
+            child.close();
+        else {
+            child.show();
+            child.setPixmap(pixmap);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        QApplication.initialize(args);
+
+        QWidget mainWidget = new QWidget();
+        QHBoxLayout horizontalLayout = new QHBoxLayout();
+        horizontalLayout.addWidget(new DraggableIcons());
+        horizontalLayout.addWidget(new DraggableIcons());
+
+        mainWidget.setLayout(horizontalLayout);
+        mainWidget.setWindowTitle("Draggable Icons");
+        mainWidget.show();
+
+        QApplication.exec();
+    }
+}
---
+++ com/trolltech/examples/DraggableText.java
@@ -0,0 +1,139 @@
+package com.trolltech.examples;
+
+import com.trolltech.qt.*;
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+
+@QtJambiExample(name = "Draggable Text")
+public class DraggableText extends QWidget
+{
+    public DraggableText()
+    {
+        QFile dictionaryFile = new QFile("classpath:com/trolltech/examples/dictionary/words.txt");
+        dictionaryFile.open(QIODevice.OpenModeFlag.ReadOnly);
+        QTextStream inputStream = new QTextStream(dictionaryFile);
+
+        int x = 5;
+        int y = 5;
+
+        while (!inputStream.atEnd()) {
+            String word = inputStream.readString();
+            if (!word.equals("")) {
+                DragLabel wordLabel = new DragLabel(word, this);
+                wordLabel.move(x, y);
+                wordLabel.show();
+                x += wordLabel.width() + 2;
+                if (x >= 195) {
+                    x = 5;
+                    y += wordLabel.height() + 2;
+                }
+            }
+        }
+
+        QPalette newPalette = palette();
+        newPalette.setColor(QPalette.ColorRole.Window, QColor.white);
+        setPalette(newPalette);
+
+        setAcceptDrops(true);
+        setMinimumSize(400, Math.max(200, y));
+        setWindowTitle(tr("Draggable Text"));
+}
+    protected void dragEnterEvent(QDragEnterEvent event)
+    {
+        if (event.mimeData().hasText()) {
+            if (children().contains(event.source())) {
+                event.setDropAction(Qt.DropAction.MoveAction);
+                event.accept();
+            } else {
+                event.acceptProposedAction();
+            }
+        } else {
+            event.ignore();
+        }
+    }
+
+    protected void dropEvent(QDropEvent event)
+    {
+        if (event.mimeData().hasText()) {
+            com.trolltech.qt.core.QMimeData mime = event.mimeData();
+            String pieces[] = mime.text().split("\\s+");
+            QPoint position = event.pos();
+            QPoint hotSpot = new QPoint();
+
+            String hotSpotPos[] = mime.data("application/x-hotspot").toString().split("\\s");
+            if (hotSpotPos.length == 2) {
+                hotSpot.setX(Integer.parseInt(hotSpotPos[0]));
+                hotSpot.setY(Integer.parseInt(hotSpotPos[1]));
+            }
+
+            for (String piece : pieces) {
+                DragLabel newLabel = new DragLabel(piece, this);
+                newLabel.move(position.subtract(hotSpot));
+                newLabel.show();
+
+                position.add(new QPoint(newLabel.width(), 0));
+            }
+
+            if (children().contains(event.source())) {
+                event.setDropAction(Qt.DropAction.MoveAction);
+                event.accept();
+            } else {
+                event.acceptProposedAction();
+            }
+        } else {
+            event.ignore();
+        }
+    }
+
+    class DragLabel extends QLabel
+    {
+        public DragLabel(String text, QWidget parent)
+        {
+            super(parent);
+
+            setText(text);
+            setAutoFillBackground(true);
+            setFrameShape(QFrame.Shape.Panel);
+            setFrameShadow(QFrame.Shadow.Raised);
+        }
+
+        protected void mousePressEvent(QMouseEvent event)
+        {
+            QPoint hotSpot = event.pos();
+
+            com.trolltech.qt.core.QMimeData mimeData = new com.trolltech.qt.core.QMimeData();
+            mimeData.setText(text());
+            mimeData.setData("application/x-hotspot",
+                              new QByteArray(String.valueOf(hotSpot.x())
+                              + " " + String.valueOf(hotSpot.y())));
+
+            QPixmap pixmap = new QPixmap(size());
+            render(pixmap);
+
+            QDrag drag = new QDrag(this);
+            drag.setMimeData(mimeData);
+            drag.setPixmap(pixmap);
+            drag.setHotSpot(hotSpot);
+
+            Qt.DropAction dropAction = drag.exec(
+                new Qt.DropActions(Qt.DropAction.CopyAction,
+                                 Qt.DropAction.MoveAction,
+                                 Qt.DropAction.CopyAction));
+
+            if (dropAction == Qt.DropAction.MoveAction) {
+                close();
+                update();
+            }
+        }
+    }
+
+    public static void main(String args[])
+    {
+        QApplication.initialize(args);
+
+        new DraggableText().show();
+
+        QApplication.exec();
+    }
+
+}
