13a4d9e92be797749b8951b8e1d8f288f6b73584
gunnar <gunnar@THAUMASIA>
2008-08-29 12:28:00 +0100

    p4i integration Integrate 316912 from 4.4 to main: Change 316912 by gunnar@gunnar-chiana on 2008/08/29 13:27:32
    p4i integration
    Integrate 316911 from 4.4.2_01 to 4.4:
    Change 316911 by gunnar@gunnar-chiana on 2008/08/29 13:27:28
    
    make it possible to use set_env from directories that have spaces...
    
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 316914]

--- set_qtjambi_env.bat
+++ set_qtjambi_env.bat
@@ -1,11 +1,11 @@
 @echo off
 
-dir /b %~dp0qtjambi-4*.jar > .tmp
+dir /b "%~dp0qtjambi-4*.jar" > .tmp
 if not "%errorlevel%" == "0" goto error
 set /p QTJAMBI_JAR=< .tmp
 set QTJAMBI_VERSION=%QTJAMBI_JAR:~8,8%
 
-dir /b %~dp0qtjambi-win*.jar > .tmp
+dir /b "%~dp0qtjambi-win*.jar" > .tmp
 set /p QTJAMBI_PLATFORM_JAR=< .tmp
 
 set CLASSPATH=%~dp0%QTJAMBI_JAR%;%~dp0%QTJAMBI_PLATFORM_JAR%;%~dp0qtjambi-examples-%QTJAMBI_VERSION%.jar;%classpath%
