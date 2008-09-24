2c1d729195c07f4976c610db73c92453cc867221
gunnar <gunnar@THAUMASIA>
2008-09-24 15:32:38 +0100

    p4i integration Integrate 318953 from 4.4 to main: Integrate 317811 from 4.4.2_01 to 4.4: activeqt wasn't built because of some bad QT_EDITION matching. Don't understand how this used to work, but at least this should fix it...
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 318954]

--- scripts/build_qt_windows.bat
+++ scripts/build_qt_windows.bat
@@ -32,6 +32,8 @@ echo #endif // __cplusplus >> src\corelib\global\qglobal.h
 echo #endif // AWESOME_CRAXX >> src\corelib\global\qglobal.h
 echo CONFIG+=force_embed_manifest >> .qmake.cache
 REM Not running "cd src && nmake" simply because there is no master makefile in src, only sub makefiles
+REM Force re-qmake ActiveQt without QT_EDITION=OpenSource
+cd src\activeqt && qmake -r QT_EDITION=Internal && cd ..\..
 nmake sub-src sub-tools
 REM deleting explicitly because "nmake clean" would fail when it got to examples...
 del *.obj *.ilk *.pdb moc_* *.pch /s
