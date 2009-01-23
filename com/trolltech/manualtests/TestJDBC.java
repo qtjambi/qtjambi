b9be2c5775e82485c8a6d1c39588d3cd96b37857
eblomfel <eblomfel@SpareRibs>
2009-01-23 16:27:25 +0100

    Make test more general. You can now choose between different drivers and if you set up the correct tables, you can easily add new drivers here. The derby driver needs to be installed locally (I've not set up any server for it) but the PostgreSQL should work remotely as long as qfwfq is turned on.
    [git-p4: depot-paths = "//depot/qtjambi/main/": change = 321776]

--- com/trolltech/manualtests/TestJDBC.java
+++ com/trolltech/manualtests/TestJDBC.java
@@ -4,28 +4,29 @@ import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 import com.trolltech.qt.sql.*;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class TestJDBC {
-    public static void main(String args[])  {
-        QApplication.initialize(args);
 
-        com.trolltech.qt.sql.QJdbc.initialize();
+    private enum Driver {
+        PostgreSQL,
+        Derby
+    }
 
-        try {
-            Class.forName("org.postgresql.Driver").newInstance();
-        } catch (Exception ex) { System.err.println(ex); return; }
+    static private String driverNames[] = {
+        "org.postgresql.Driver",
+        "org.apache.derby.jdbc.EmbeddedDriver"
+    };
 
-        QSqlDatabase db = QSqlDatabase.addDatabase("QJDBC");
-        db.setDatabaseName("jdbc:postgresql://localhost/main");
-        db.setUserName("qt");
-        db.setPassword("qqqqqq");
-        if (db.open()) {
-            System.out.println("Connected!");
-        } else {
-            System.out.println("Connection Failed!");
-            System.out.println(db.lastError().text());
-            return;
-        }
+    static private String dbNames[] = {
+        "jdbc:postgresql://qfwfq.europe.nokia.com/main",
+        "jdbc:derby:firstdb" // only usable locally, since no server is set up. Install derby and make the
+                             // database on your local machine to run this test.
+    };
 
+    private static void makeRelationalTable(QTabWidget topLevel, QSqlDatabase db)  {
         QSqlRelationalTableModel model = new QSqlRelationalTableModel(null, db);
         model.setTable("person");
         model.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange);
@@ -34,9 +35,73 @@ public class TestJDBC {
             System.err.println(model.lastError().text());
         }
         QTableView view = new QTableView();
+        view.setWindowTitle("QSqlRelationalTableModel");
         view.setModel(model);
         view.setItemDelegate(new QSqlRelationalDelegate(view));
-        view.show();
+        topLevel.addTab(view, "QSqlRelationalTableModel");
+    }
+
+    private static void makeRegularTable(QTabWidget topLevel, QSqlDatabase db)  {
+        QSqlTableModel model = new QSqlTableModel(null, db);
+        model.setTable("person");
+        model.select();
+        QTableView view = new QTableView(topLevel);
+        view.setModel(model);
+        topLevel.addTab(view, "person");
+
+        model = new QSqlTableModel(null, db);
+        model.setTable("country");
+        model.select();
+        view = new QTableView(topLevel);
+        view.setModel(model);
+        topLevel.addTab(view, "country");
+    }
+
+    public static void main(String args[])  {
+        QApplication.initialize(args);
+
+        com.trolltech.qt.sql.QJdbc.initialize();
+
+        List<String> items = new ArrayList<String>();
+        for (Driver driver : Driver.values())
+            items.add(driver.name());
+
+        String selectedDriver = QInputDialog.getItem(null, "Select a driver", "Select a driver", items);
+        int ordinal = -1;
+
+        for (Driver driver : Driver.values()) {
+            if (driver.name().equals(selectedDriver))
+                ordinal = driver.ordinal();
+        }
+
+        if (ordinal < 0)
+            return;
+
+        try {
+            System.err.println("Loading driver '" + driverNames[ordinal] + "'");
+            Class.forName(driverNames[ordinal]).newInstance();
+        } catch (Exception ex) { System.err.println(ex); return; }
+
+        QSqlDatabase db = QSqlDatabase.addDatabase("QJDBC");
+        System.err.println("Selecting database '" + dbNames[ordinal] + "'");
+        db.setDatabaseName(dbNames[ordinal]);
+        db.setUserName("qt");
+        db.setPassword("qqqqqq");
+        if (db.open()) {
+            System.err.println("Connected!");
+        } else {
+            System.err.println("Connection Failed!");
+            System.err.println(db.lastError().text());
+            return;
+        }
+
+        QTabWidget topLevel = new QTabWidget();
+        topLevel.setWindowTitle("Tables");
+
+        makeRelationalTable(topLevel, db);
+        makeRegularTable(topLevel, db);
+
+        topLevel.show();
 
         QApplication.exec();
 
