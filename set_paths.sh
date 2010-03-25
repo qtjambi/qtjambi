d2a441877ddf3f0433403223351398105a750c1c
Peter Droste <pedro@anyvis.(none)>
2010-03-25 10:29:14 +0100

    new file:   set_paths.sh

---
+++ set_paths.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export QTDIR=/home/pedro/Programmierung/Qt/Qt4.6.2_linux64
+export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.15
+export QTJAMBI=/home/pedro/Programmierung/Java/pedro79s_qtjambi_4_6
+export PATH=$QTDIR/bin:$PATH
+
+bash
