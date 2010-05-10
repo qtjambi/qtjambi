581531c3954bc61d9169d027524415b4af523fa4
Samu Voutilainen <smar@smar.fi>
2010-05-10 15:18:16 +0300

    export fix

--- qtjambi/qtjambifunctiontable.h
+++ qtjambi/qtjambifunctiontable.h
@@ -10,7 +10,7 @@
 ** accordance with the Qt Commercial License Agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and Nokia.
-** 
+**
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
 ** General Public License version 2.1 as published by the Free Software
@@ -18,12 +18,12 @@
 ** packaging of this file.  Please review the following information to
 ** ensure the GNU Lesser General Public License version 2.1 requirements
 ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-** 
+**
 ** In addition, as a special exception, Nokia gives you certain
 ** additional rights. These rights are described in the Nokia Qt LGPL
 ** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
 ** package.
-** 
+**
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
 ** General Public License version 3.0 as published by the Free Software
@@ -31,7 +31,7 @@
 ** packaging of this file.  Please review the following information to
 ** ensure the GNU General Public License version 3.0 requirements will be
 ** met: http://www.gnu.org/copyleft/gpl.html.
-** 
+**
 ** If you are unsure which license is appropriate for your use, please
 ** contact the sales department at qt-sales@nokia.com.
 ** $END_LICENSE$
@@ -45,6 +45,7 @@
 #ifndef QTJAMBIFUNCTIONTABLE_H
 #define QTJAMBIFUNCTIONTABLE_H
 
+#define QTJAMBI_EXPORT
 #include "qtjambi_global.h"
 
 #include <QtCore/QObject>
@@ -73,22 +74,5 @@ private:
 
     int m_reference_count;
 };
-
-
-QTJAMBI_EXPORT jmethodID QtJambiFunctionTable::method(int pos) const
-{
-    Q_ASSERT(pos >= 0);
-    Q_ASSERT(pos < m_method_count);
-    return m_method_ids[pos];
-}
-
-
-QTJAMBI_EXPORT void QtJambiFunctionTable::setMethod(int pos, jmethodID id)
-{
-    Q_ASSERT(pos >= 0);
-    Q_ASSERT(pos < m_method_count);
-    m_method_ids[pos] = id;
-}
-
 
 #endif // QTJAMBIFUNCTIONTABLE_H
