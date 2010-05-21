75d162034856bee682e3b038763e987d4dbed995
Samu Voutilainen <smar@smar.fi>
2010-05-21 15:58:45 +0300

    fix phonon for QTDIR...

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 export QTDIR=/home/smar/Paketit/qt/qt
 #export QTDIR=/usr/lib/qt4
 #export JAVA_HOME=/java/path  
-export QTJAMBI=$PWD  
+#export QTJAMBI=$PWD  
 export PATH=$QTDIR/bin:$PATH  
-export LD_LIBRARY_PATH=$QTDIR/lib
+#export LD_LIBRARY_PATH=$QTDIR/lib
 #export LD_LIBRARY_PATH=/usr/lib/qt4
 ant $1
