c3297846c2ef2e192900ec80a2319cc75f496914
Samu Voutilainen <smar@smar.fi>
2010-05-24 10:25:57 +0300

    fix phonon plugin bundling for windows

--- java/src/qtjambi/com/trolltech/qt/internal/NativeLibraryManager.java
+++ java/src/qtjambi/com/trolltech/qt/internal/NativeLibraryManager.java
@@ -54,6 +54,8 @@ import javax.xml.parsers.*;
 
 import org.xml.sax.helpers.*;
 
+import sun.awt.OSInfo;
+
 import com.trolltech.qt.Utilities;
 
 // !!NOTE!! This class can have no dependencies on Qt since
