76d59ecfdcb0c560e24721b869045d8477fedf1a
Akos Kemives <akoskm@gmail.com>
2010-12-04 16:30:58 +0100

    Unit Test implementations for Utilities.java

---
+++ src/java/test/com/trolltech/qt/tests/UtilitiesTest.java
@@ -0,0 +1,34 @@
+/**
+ * Unit Test implementations for Utilities.java
+ * 
+ * @author akoskm
+ */
+
+package com.trolltech.qt.tests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.trolltech.qt.Utilities;
+
+public class UtilitiesTest extends TestCase {
+
+    public UtilitiesTest(String name) {
+	super(name);
+    }
+
+    public void setUp() {
+	
+    }
+    
+    public void tearDown() {
+	
+    }
+    
+    public static Test suite() {
+	TestSuite suite = new TestSuite();
+	return suite;
+    }
+    
+}
