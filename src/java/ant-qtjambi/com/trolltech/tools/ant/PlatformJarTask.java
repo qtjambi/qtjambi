ad9444fbdef8c1476a59deb246b515410d9c1523
Samu Voutilainen <smar@smar.fi>
2010-12-17 21:55:26 +0200

    Removed broken rpath generation code. Fixes bug #70

--- src/java/ant-qtjambi/com/trolltech/tools/ant/PlatformJarTask.java
+++ src/java/ant-qtjambi/com/trolltech/tools/ant/PlatformJarTask.java
@@ -170,8 +170,6 @@ public class PlatformJarTask extends Task {
         if ( rpath ) {
             if ( OSInfo.os() == OSInfo.OS.MacOS ) {
                 processOSXInstallName();
-            } else if ( OSInfo.os() == OSInfo.OS.Linux ) {
-                processRPath();
             }
         }
 
@@ -407,45 +405,6 @@ public class PlatformJarTask extends Task {
         System.out.println();
 
     }
-
-    private void processRPath() {
-        System.out.println ( "Processing RPATH..." );
-        String cmd[] = new String[] {
-            "chrpath",
-            "--replace",
-            null,               // New RPATH
-            null                // Binary to update...
-        };
-
-        //String jambilibdir = props.getProperty ( ( String ) null, InitializeTask.JAMBILIBDIR ).toString();
-
-        try {
-        for ( LibraryEntry lib : libs ) {
-                System.out.println ( " - updating: " + lib.getName() );
-
-                String subdir = lib.getRootpath().toString();
-                //int dotdotCount = subdir.split ( "/" ).length;
-                StringBuilder builder = createDotDots ( subdir );
-
-                builder.insert ( 0, "$ORIGIN/" );
-                builder.append ( "lib" );
-
-                cmd[2] = builder.toString();
-                //cmd[3] = lib.relativePath();
-                cmd[3] = lib.getName();
-
-                Exec.exec ( cmd, outdir, true );
-            }
-        } catch ( Exception e ) {
-            System.out.println ( " - " + e.getMessage() );
-            System.out.println ( "\n********** Warning **********" );
-            System.out.println ( "Without rpaths, you run the risk that Qt applications and plugins" );
-            System.out.println ( "load incorrect Qt libraries (such as /usr/lib/libQtCore.so), which" );
-            System.out.println ( "may result in binary incompatility and crashes." );
-            System.out.println ( "*****************************\n" );
-        }
-    }
-
 
     private void processOSXInstallName() {
         System.out.println ( "Processing Mac OS X install_name..." );
