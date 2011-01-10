574b73c26397948609a843cd719309ef2c090242
Samu Voutilainen <smar@smar.fi>
2011-01-10 19:38:49 +0200

    reindented

--- generator/metajavabuilder.h
+++ generator/metajavabuilder.h
@@ -10,7 +10,7 @@
 ** accordance with the Qt Commercial License Agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
 ** a written agreement between you and Nokia.
-** 
+**
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
 ** General Public License version 2.1 as published by the Free Software
@@ -18,12 +18,12 @@
 ** packaging of this file.  Please review the following information to
 ** ensure the GNU Lesser General Public License version 2.1 requirements
 ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-** 
+**
 ** In addition, as a special exception, Nokia gives you certain
 ** additional rights. These rights are described in the Nokia Qt LGPL
 ** Exception version 1.0, included in the file LGPL_EXCEPTION.txt in this
 ** package.
-** 
+**
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
 ** General Public License version 3.0 as published by the Free Software
@@ -31,7 +31,7 @@
 ** packaging of this file.  Please review the following information to
 ** ensure the GNU General Public License version 3.0 requirements will be
 ** met: http://www.gnu.org/copyleft/gpl.html.
-** 
+**
 ** If you are unsure which license is appropriate for your use, please
 ** contact the sales department at qt-sales@nokia.com.
 ** $END_LICENSE$
@@ -48,42 +48,34 @@
 #include "abstractmetabuilder.h"
 #include "metajava.h"
 
-class MetaJavaBuilder : public AbstractMetaBuilder
-{
+class MetaJavaBuilder : public AbstractMetaBuilder {
 
- protected:
-    virtual MetaJavaClass *createMetaClass()
-        {
+    protected:
+        virtual MetaJavaClass *createMetaClass() {
             return new MetaJavaClass();
         };
 
-    virtual MetaJavaEnum *createMetaEnum()
-        {
+        virtual MetaJavaEnum *createMetaEnum() {
             return new MetaJavaEnum();
         };
 
-    virtual MetaJavaEnumValue *createMetaEnumValue()
-        {
+        virtual MetaJavaEnumValue *createMetaEnumValue() {
             return new MetaJavaEnumValue();
         };
 
-    virtual MetaJavaField *createMetaField()
-        {
+        virtual MetaJavaField *createMetaField() {
             return new MetaJavaField();
         };
 
-    virtual MetaJavaFunction *createMetaFunction()
-        {
+        virtual MetaJavaFunction *createMetaFunction() {
             return new MetaJavaFunction();
         };
 
-    virtual MetaJavaArgument *createMetaArgument()
-        {
+        virtual MetaJavaArgument *createMetaArgument() {
             return new MetaJavaArgument();
         };
 
-    virtual MetaJavaType *createMetaType()
-        {
+        virtual MetaJavaType *createMetaType() {
             return new MetaJavaType();
         };
 
