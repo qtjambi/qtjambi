f62b151aa4acf4080c9c0ae25df9a10839e8ad25
Akos Kemives <akoskm@gmail.com>
2011-06-23 23:26:45 +0200

    Test for QtScript Javascript-side functionalities

---
+++ src/java/autotest/com/trolltech/unittests/TestQScriptEngine.java
@@ -0,0 +1,50 @@
+/**
+ * Unit Test implementations for QScriptEngine.java
+ * 
+ * @author akoskm
+ */
+
+package com.trolltech.unittests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.script.QScriptEngine;
+import com.trolltech.qt.script.QScriptValue;
+
+public class TestQScriptEngine extends TestCase {
+    
+    QScriptEngine testEngine;
+    QScriptValue testValue;
+    
+    public TestQScriptEngine(String name) {
+	super(name);
+    }
+    
+    public void setUp() throws Exception {
+	QApplication.initialize(new String[] {});
+	testEngine = new QScriptEngine();
+    }
+    
+    public void tearDown() throws Exception {
+	testEngine = null;
+	QApplication.quit();
+	QApplication.instance().dispose();
+    }
+    
+    public void testEvaluate() {
+	assertTrue(testEngine.evaluate("1 + 2").toString().equals("3"));
+	assertTrue(testEngine.evaluate("1 + 2").toInteger() == 3);
+	assertTrue(testEngine.evaluate("1 == 2").toBoolean() == false);
+	assertTrue(testEngine.evaluate("var res = 1; for(var i = 1; i <= 5; i++)res = res*i;").toInt32() == 120);
+    }
+        
+    public static Test suite() {
+	TestSuite suite = new TestSuite();
+	suite.addTest(new TestQScriptEngine("testEvaluate"));
+	return suite;
+    }
+    
+}
