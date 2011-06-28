b49ad60f4036aa0f6b9405273c7382c207bbf765
Akos Kemives <akoskm@gmail.com>
2011-06-28 23:49:24 +0200

    add property and setProperty tests

--- src/java/autotest/com/trolltech/unittests/TestQScriptEngine.java
+++ src/java/autotest/com/trolltech/unittests/TestQScriptEngine.java
@@ -11,6 +11,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QPushButton;
 import com.trolltech.qt.script.QScriptEngine;
 import com.trolltech.qt.script.QScriptValue;
 
@@ -18,6 +19,8 @@ public class TestQScriptEngine extends TestCase {
     
     QScriptEngine testEngine;
     QScriptValue testValue;
+    QPushButton button;
+    QScriptValue scriptButton;
     
     public TestQScriptEngine(String name) {
 	super(name);
@@ -26,10 +29,15 @@ public class TestQScriptEngine extends TestCase {
     public void setUp() throws Exception {
 	QApplication.initialize(new String[] {});
 	testEngine = new QScriptEngine();
+	button = new QPushButton();
+	scriptButton = testEngine.newQObject(button);
+	testEngine.globalObject().setProperty("button", scriptButton);
     }
     
     public void tearDown() throws Exception {
 	testEngine = null;
+	button = null;
+	scriptButton = null;
 	QApplication.quit();
 	QApplication.instance().dispose();
     }
@@ -40,10 +48,22 @@ public class TestQScriptEngine extends TestCase {
 	assertTrue(testEngine.evaluate("1 == 2").toBoolean() == false);
 	assertTrue(testEngine.evaluate("var res = 1; for(var i = 1; i <= 5; i++)res = res*i;").toInt32() == 120);
     }
-        
+    
+    public void testSetProperty() {
+	scriptButton.setProperty("checkable", new QScriptValue(true));
+	assertTrue(testEngine.evaluate("button.checkable = true").toBoolean());
+    }
+    
+    public void testProperty() {
+	scriptButton.setProperty("checkable", new QScriptValue(true));	
+	assertTrue(scriptButton.property("checkable").toBoolean());
+    }
+    	
     public static Test suite() {
 	TestSuite suite = new TestSuite();
 	suite.addTest(new TestQScriptEngine("testEvaluate"));
+	suite.addTest(new TestQScriptEngine("testSetProperty"));
+	suite.addTest(new TestQScriptEngine("testProperty"));
 	return suite;
     }
     
