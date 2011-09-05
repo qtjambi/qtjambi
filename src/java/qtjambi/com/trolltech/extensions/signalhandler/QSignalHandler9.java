9da8b024c56f454333a87c6dc77e1ca6609738bd
Darryl L. Miles <darryl.miles@darrylmiles.org>
2011-09-05 09:32:58 +0100

    Correct comment typos.

--- src/java/qtjambi/com/trolltech/extensions/signalhandler/QSignalHandler9.java
+++ src/java/qtjambi/com/trolltech/extensions/signalhandler/QSignalHandler9.java
@@ -65,12 +65,12 @@ public abstract class QSignalHandler9<A, B, C, D, E, F, G, H, I> {
      * @param arg1 The first signal argument.
      * @param arg2 The second signal argument.
      * @param arg3 The third signal argument.
-     * @param arg4 The first signal argument.
-     * @param arg5 The second signal argument.
-     * @param arg6 The third signal argument.
-     * @param arg7 The first signal argument.
-     * @param arg8 The second signal argument.
-     * @param arg9 The third signal argument.
+     * @param arg4 The fourth signal argument.
+     * @param arg5 The fifth signal argument.
+     * @param arg6 The sixth signal argument.
+     * @param arg7 The seventh signal argument.
+     * @param arg8 The eighth signal argument.
+     * @param arg9 The ninth signal argument.
      */
     public abstract void handle(A arg1, B arg2, C arg3, D arg4, E arg5, F arg6, G arg7, H arg8, I arg9);
 }
