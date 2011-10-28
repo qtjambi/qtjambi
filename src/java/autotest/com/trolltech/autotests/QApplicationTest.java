ff40952a0a96cf44263382eede6ff4ec99396831
Darryl L. Miles <darryl.miles@darrylmiles.org>
2011-10-28 14:53:50 +0100

    Testcase: QApplicationText, improvements to testcase lifecycle and logging.

--- src/java/autotest/com/trolltech/autotests/QApplicationTest.java
+++ src/java/autotest/com/trolltech/autotests/QApplicationTest.java
@@ -49,29 +49,104 @@ import org.junit.BeforeClass;
 
 import com.trolltech.qt.QNoNativeResourcesException;
 import com.trolltech.qt.QSignalEmitter;
+import com.trolltech.qt.core.QEventLoop;
+import com.trolltech.qt.core.QMessageHandler;
 import com.trolltech.qt.gui.QApplication;
 
 public abstract class QApplicationTest extends QSignalEmitter {
+    public static class MyQMessageHandler extends QMessageHandler {
+        @Override
+        public void debug(String message) {
+            System.out.println("DEBUG: " + message);
+            System.out.flush();
+        }
+
+        @Override
+        public void warning(String message) {
+            System.out.println("WARN: " + message);
+            System.out.flush();
+        }
+
+        @Override
+        public void critical(String message) {
+            System.out.println("CRIT: " + message);
+            System.out.flush();
+        }
+
+        @Override
+        public void fatal(String message) {
+            System.out.println("FATAL: " + message);
+            System.out.flush();
+        }
+    }
+
+    private static QMessageHandler myQMessageHandler;
+
     @BeforeClass
     public static void testInitialize() throws Exception {
         Utils.println(2, "QApplicationTest.testInitialize(): begin");
         QApplication.initialize(new String[] {});
         Utils.setupNativeResourceThread();
-        Utils.println(2, "QApplicationTest.testInitialize(): done");    
+        myQMessageHandler = new MyQMessageHandler();
+        QMessageHandler.installMessageHandler(myQMessageHandler);
+        Utils.println(2, "QApplicationTest.testInitialize(): done");
     }
 
     @AfterClass
     public static void testDispose() throws Exception {
-        QApplication.quit();
+        Utils.println(2, "QApplicationTest.testDispose(): begin");
+        System.gc();
+        System.runFinalization();
+
+        // We are attempting to reach a point here where memory should be reclaimed
+        // except for anything linked to QApplication.
+
+        QApplication app = QApplication.instance();
+        try {
+            Utils.println(3, "QApplicationTest.testDispose(): QApplication-only  app="+app);
+        } catch(QNoNativeResourcesException e) {
+            Utils.println(3, "QApplicationTest.testDispose(): QApplication-only  com.trolltech.qt.QNoNativeResourcesException: app="+e.getMessage());
+        }
+
         System.err.flush();
         System.out.flush();
-        QApplication app = QApplication.instance();
-        if(app != null)
-            app.dispose();
-        try {
-            Utils.println(3, "QApplicationTest.testDispose(): done  app="+app); 
-        } catch(QNoNativeResourcesException e) {
-            Utils.println(3, "QApplicationTest.testDispose(): done  com.trolltech.qt.QNoNativeResourcesException: app="+e.getMessage()); 
+
+        if(app != null) {
+            try {
+                QApplication.processEvents();	// NPE
+            } catch(Throwable t) {
+                t.printStackTrace();
+            }
+            QApplication.processEvents(QEventLoop.ProcessEventsFlag.DeferredDeletion);	// NPE
         }
+        QApplication.quit();
+        app = QApplication.instance();
+        if(app != null) {
+            QApplication.processEvents();
+            QApplication.processEvents(QEventLoop.ProcessEventsFlag.DeferredDeletion);
+            app.dispose();
+            try {
+                Utils.println(3, "QApplicationTest.testDispose(): done  app="+app);
+            } catch(QNoNativeResourcesException e) {
+                Utils.println(3, "QApplicationTest.testDispose(): done  com.trolltech.qt.QNoNativeResourcesException: app="+e.getMessage());
+            }
+            app = null;		// kill hard-reference
+        }
+        if(myQMessageHandler != null) {
+            QMessageHandler.removeMessageHandler(myQMessageHandler);
+            myQMessageHandler = null;
+        }
+        System.gc();
+        System.runFinalization();
+
+        // We are attempting to reach a point here where memory should be reclaimed
+        // including that which was linked to QApplication
+
+        Utils.println(3, "QApplicationTest.testDispose(): shutdown PRE");
+        QApplication.shutdown();
+        Utils.println(3, "QApplicationTest.testDispose(): shutdown POST");
+
+        int objectCount = QtJambiUnittestTools.getObjectCount(1, 0);
+        Utils.println(3, "QApplicationTest.testDispose(): end objectCount="+objectCount);
     }
 }
