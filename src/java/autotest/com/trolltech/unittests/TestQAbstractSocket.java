b5da17a1f2c439d5742d5e97ad31cf04db0dd5b3
Akos Kemives <akoskm@gmail.com>
2012-02-18 09:26:47 +0100

    TestQAbstractSocket.java: basic socket test

---
+++ src/java/autotest/com/trolltech/unittests/TestQAbstractSocket.java
@@ -0,0 +1,31 @@
+package com.trolltech.unittests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.trolltech.qt.core.QObject;
+import com.trolltech.qt.network.QAbstractSocket;
+
+public class TestQAbstractSocket {
+
+	QAbstractSocket socket = null;
+	
+	@Before
+	public void setUp() throws Exception {
+		socket = new QAbstractSocket(QAbstractSocket.SocketType.TcpSocket, new QObject());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConnectToHost() {
+		socket.connectToHost("http://www.google.com", 80);
+		assertTrue(socket.isValid());
+	}
+
+}
