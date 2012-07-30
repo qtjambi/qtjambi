f522ad3378fd6b9c36caf46c853467cc18237b42
Sergio Ahumada <sergio.ahumada@nokia.com>
2012-07-30 11:52:18 +0200

    Update contact information in license headers.
    - Replace Nokia contact email address with Qt Project website.
    - Remove "All rights reserved" line from license headers.
    
    As in the past, to avoid rewriting various autotests that contain
    line-number information, an extra blank line has been inserted at the
    end of the license text to ensure that this commit does not change the
    total number of lines in the license header.
    
    Change-Id: Ie7ba62011752fcb149b99b26317c54f2a0cfa931
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    Signed-off-by: Joni <joao267@gmail.com>
    Reviewed-on: http://gerrit.smar.fi/315
    Reviewed-by: Samu Voutilainen <smar@smar.fi>

--- juic/uic/cpp/cppwriteicondata.h
+++ juic/uic/cpp/cppwriteicondata.h
@@ -1,8 +1,7 @@
 /****************************************************************************
 **
 ** Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
-** All rights reserved.
-** Contact: Nokia Corporation (qt-info@nokia.com)
+** Contact: http://www.qt-project.org/
 **
 ** This file is part of the tools applications of the Qt Toolkit.
 **
@@ -35,6 +34,7 @@
 **
 ** If you have questions regarding the use of this file, please contact
 ** Nokia at qt-info@nokia.com.
+**
 **
 **
 **
