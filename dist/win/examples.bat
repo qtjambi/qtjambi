579cbe4b13661ad5bff10050bc8809a59cd06055
Samu Voutilainen <smar@smar.fi>
2013-08-25 12:45:41 +0300

    Modify examples.bat to include examples source jar.
    Change-Id: I82f19e9d94f85508af7a465164b2e3ed5ef4f63a
    Reviewed-on: http://gerrit.smar.fi/237
    Reviewed-by: João Francisco Almeida <joao267@gmail.com>
    Reviewed-by: Samu Voutilainen <smar@smar.fi>

--- dist/win/examples.bat
+++ dist/win/examples.bat
@@ -7,8 +7,10 @@ set VERSION=%QTJAMBI_JAR:~8,5%
 set QT_PLUGIN_PATH=%BASE_DIR%\plugins
 set BIN_PATH=%BASE_DIR%\bin
 set PATH=%BIN_PATH%;%PATH%
+REM examples src is used for source browsing and needs to be loaded before actual examples jar
 set CLASSPATH=%BASE_DIR%\qtjambi-%VERSION%.jar;^
 %BASE_DIR%\qtjambi-native-win32-mingw-%VERSION%.jar;^
+%BASE_DIR%\qtjambi-examples-src-%VERSION%.jar;^
 %BASE_DIR%\qtjambi-examples-%VERSION%.jar
 
 REM Launch demo
