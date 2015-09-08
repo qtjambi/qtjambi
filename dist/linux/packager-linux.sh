8ef99da63315945e6ab540cc31d66e5b021b69e4
João Almeida <joao267@gmail.com>
2015-09-08 19:28:46 +0100

    dist: Clean and improve Linux and OSX Packager scripts
    * Make all packager more uniform between platforms
    * Remove qtjambi-util.jar
    * Add qtjambi-src.jar
    
    Change-Id: Iab5b0bf2720c810658006619fafaa21be7975f8c
    Reviewed-on: http://gerrit.smar.fi/1303
    Reviewed-by: Samu Voutilainen <smar@smar.fi>

--- dist/linux/packager-linux.sh
+++ dist/linux/packager-linux.sh
@@ -10,39 +10,47 @@ VERSION=${VERSION:8:5}
 
 OS=$(ls build/qtjambi-native-*.jar | cut -sd / -f 2-)
 OS=${OS:15:7}
-
-RELEASE=qtjambi-$OS-community-$VERSION
 COMPILER=gcc
 
+RELEASE=qtjambi-$OS-community-$VERSION
+
+# Clear previous attempts
+rm -Rf $RELEASE $RELEASE.tar.gz
+
+# Create temporary folders
 mkdir $RELEASE
 mkdir $RELEASE/bin
 
-#cp version.properties $RELEASE
-
+# Jars:
 cp qtjambi-$VERSION.jar $RELEASE
 cp qtjambi-designer-$VERSION.jar $RELEASE
 cp qtjambi-examples-$VERSION.jar $RELEASE
 cp jars/qtjambi-examples-src.jar $RELEASE
+cp jars/qtjambi-src.jar $RELEASE
 cp build/qtjambi-native-$OS-$COMPILER-$VERSION.jar $RELEASE
 
+# Shell scripts:
 cp dist/linux/examples.sh $RELEASE
 cp dist/linux/designer.sh $RELEASE
 cp dist/linux/designer_lib_expander.sh $RELEASE
 
+# Licenses and misc
 #cp dist/install.html $RELEASE
-#cp dist/LGPL_EXCEPTION.TXT $RELEASE
+cp dist/changes-$VERSION $RELEASE
 cp dist/LICENSE.GPL3 $RELEASE
 cp dist/LICENSE.LGPL $RELEASE
-#cp dist/readme.txt $RELEASE
 
+# Juic
 cp build/qmake-juic/juic $RELEASE/bin
 
+# Qt Designer and other utilities
 cp $QTDIR/bin/designer $RELEASE/bin
 cp $QTDIR/bin/linguist $RELEASE/bin
 cp $QTDIR/bin/lrelease $RELEASE/bin
 cp $QTDIR/bin/lupdate $RELEASE/bin
 
-
+# Pack everything together
 tar czf $RELEASE.tar.gz $RELEASE
 
+# Remove temporary folder
 rm -Rf $RELEASE
--- dist/mac/packager-osx.sh
+++ dist/mac/packager-osx.sh
@@ -5,44 +5,52 @@ if [ "${QTDIR}" == "" ]; then
     exit 1
 fi
 
-OS=macosx
 VERSION=$(ls qtjambi-4*.jar)
 VERSION=${VERSION:8:5}
-RELEASE=qtjambi-$OS-community-$VERSION
-COMPILER=gcc
-JAMBISTUFF=build/platform-output
 
+OS=macosx
+COMPILER=gcc
+
+RELEASE=qtjambi-$OS-community-$VERSION
+
+# Clear previous attempts
+rm -Rf $RELEASE $RELEASE.tar.gz
+
+# Create temporary folders:
 mkdir $RELEASE
 mkdir $RELEASE/bin
 
-cp version.properties $RELEASE
-
+# Jars:
 cp qtjambi-$VERSION.jar $RELEASE
 cp qtjambi-designer-$VERSION.jar $RELEASE
 cp qtjambi-examples-$VERSION.jar $RELEASE
 cp jars/qtjambi-examples-src.jar $RELEASE
-cp jars/qtjambi-util.jar $RELEASE
+cp jars/qtjambi-src.jar $RELEASE
 cp build/qtjambi-native-$OS-$COMPILER-$VERSION.jar $RELEASE
 
+# Shell scripts:
 cp dist/mac/qtjambi.sh $RELEASE
 cp dist/mac/designer.sh $RELEASE
 cp dist/mac/juic.sh $RELEASE
 cp dist/mac/installer-osx.sh $RELEASE
-#cp set_qtjambi_env.sh $RELEASE
+
+# Licenses and misc:
+#cp dist/install.html $RELEASE
 cp dist/changes-$VERSION $RELEASE
-cp dist/install.html $RELEASE
-cp dist/LGPL_EXCEPTION.TXT $RELEASE
 cp dist/LICENSE.GPL3 $RELEASE
 cp dist/LICENSE.LGPL $RELEASE
-cp dist/readme.txt $RELEASE
 
+# Juic
 cp build/qmake-juic/juic $RELEASE/bin
+
+# Qt Designer and other utilities:
 cp -R $QTDIR/bin/Designer.app $RELEASE/bin
 cp -R $QTDIR/bin/Linguist.app $RELEASE/bin
 cp $QTDIR/bin/lrelease $RELEASE/bin
 cp $QTDIR/bin/lupdate $RELEASE/bin
 
-
+# Pack everything together
 tar czf $RELEASE.tar.gz $RELEASE
 
+# Remove temporary folder
 rm -Rf $RELEASE
